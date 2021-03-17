76312f8ff8f2d24affb733262ba8f8699e7155ae
Ewoud Dronkert <ewoud@dronkert.net>
2021-03-17 17:49:23 +0100

    It's too much. Still, it's nice.

--- fizzbuzz.c
+++ fizzbuzz.c
@@ -1,25 +1,77 @@
-#include <stdio.h>
+#include <stdio.h>   // (f)printf
+#include <stdlib.h>  // atoi
 
-#define F (3U)      // Fizz factor
-#define B (5U)      // Buzz factor
-#define N (1000U)   // count from 1 to N
-#define L (10000U)  // repeat L times
+#define MINFIZZ  (1)
+#define MAXFIZZ  (100)
+#define MAXCOUNT (1000)
 
-int main(void)
+// Least Common Multiple
+static int lcm(int a, int b)
 {
-    unsigned int i, n, f, b;
+    if (a == 0 || b == 0) {
+        return 0;
+    }
+    int p = abs(a), q = abs(b), r;
+    if (q > p) {
+        r = p;
+        p = q;
+        q = r;
+    }
+    while (q) {
+        r = p % q;
+        p = q;
+        q = r;
+    }
+    return a / p * b;
+}
 
-    for (i = 0; i < L; ++i) {
-        f = F; b = B;
-        for (n = 1; n <= N; ++n) {
-            switch (((n == b) << 1) | (n == f)) {  // 2-bit condition
-                case 0: printf("%u ", n);                    break;
-                case 1: printf("Fizz ");     f += F;         break;
-                case 2: printf("Buzz ");     b += B;         break;
-                case 3: printf("FizzBuzz "); f += F; b += B; break;
-            }
+int main(int argc, char *argv[])
+{
+    int fizz = 3, buzz = 5, count = 15;
+
+    if (argc > 4) {
+        fprintf(stderr, "\nArguments: [fizz buzz] [count]\n\n");
+        return 1;
+    } else if (argc == 2) {
+        // One argument = count
+        count = abs(atoi(argv[1]));
+    } else if (argc == 3) {
+        // Two arguments = fizz, buzz
+        fizz  = abs(atoi(argv[1]));
+        buzz  = abs(atoi(argv[2]));
+        count = lcm(fizz, buzz);
+        if (count > MAXCOUNT) {
+            count = MAXCOUNT;
         }
-        printf("\n");
+    } else if (argc == 4) {
+        // Three arguments
+        fizz  = abs(atoi(argv[1]));
+        buzz  = abs(atoi(argv[2]));
+        count = abs(atoi(argv[3]));
+    }
+    // Sanity check
+    if (fizz  < MINFIZZ || fizz  > MAXFIZZ ||
+        buzz  < MINFIZZ || buzz  > MAXFIZZ ||
+        count < MINFIZZ || count > MAXCOUNT) {
+        fprintf(stderr,
+            "\nArgument range: fizz %u..%u, buzz %u..%u, count %u..%u\n\n",
+            MINFIZZ, MAXFIZZ, MINFIZZ, MAXFIZZ, MINFIZZ, MAXCOUNT);
+        return 2;
+    }
+
+    // Summary
+    printf("Fizz  : %d\nBuzz  : %d\nCount : %d\n", fizz, buzz, count);
+
+    // Do The Thing
+    int n, f = fizz, b = buzz;
+    for (n = MINFIZZ; n <= count; ++n) {
+        switch (((n == b) << 1) | (n == f)) {          // 2-bit condition
+            case 0: printf("%u", n); break;            // no common factor
+            case 1: printf("Fizz"); f += fizz; break;  // common factor fizz
+            case 2: printf("Buzz"); b += buzz; break;  // common factor buzz
+            case 3: printf("FizzBuzz"); f += fizz; b += buzz; break;
+        }
+        printf(n < count ? " " : "\n");
     }
     return 0;
 }
