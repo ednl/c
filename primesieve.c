5667d65d6786d8127b01b48b8e517bc0ebb2320d
Ewoud Dronkert <ewoud@dronkert.net>
2021-04-07 19:20:15 +0200

    whyyy is it still slower

--- primesieve.c
+++ primesieve.c
@@ -1,16 +1,21 @@
-#include <stdio.h>    // printf
-#include <stdbool.h>  // bool, true, false
-#include <time.h>     // clock, CLOCKS_PER_SEC
+#include <stdio.h>   // printf
+#include <time.h>    // clock, CLOCKS_PER_SEC
+#include <math.h>    // sqrtf, floorf
+#include <stdlib.h>  // calloc
+#include <string.h>  // memcpy
 
-#define LIM  (1000000U)  // count number of primes which are smaller than this
-#define SIZE (LIM >> 1)  // uneven number count
-#define FIVESEC (CLOCKS_PER_SEC * 5)
+#define ISPRIME  (0U)
+#define NOTPRIME (1U)
+#define LIMIT    (1000000U)
+#define HALF     (LIMIT >> 1)
+#define FIVESEC  (CLOCKS_PER_SEC * 5)
+
+static unsigned int rooth;
 
 typedef struct {
     unsigned int limit, count;
 } VALID;
-
-const static VALID valid[] = {
+static const VALID valid[] = {
     {          10U, 4U         },
     {         100U, 25U        },
     {        1000U, 168U       },
@@ -23,58 +28,56 @@ const static VALID valid[] = {
     // { 10000000000U, 455052511U },
 };
 
-static unsigned char isprime[SIZE] = {0};  // only keep track of uneven numbers
-
-static bool isvalid(unsigned int limit, unsigned int count)
+static unsigned char isvalid(unsigned int limit, unsigned int count)
 {
-    for (unsigned int i = 0; i < sizeof(valid) / sizeof(*valid); ++i) {
+    for (size_t i = 0; i < sizeof valid / sizeof *valid; ++i) {
         if (valid[i].limit == limit) {
             return valid[i].count == count;
         }
     }
-    return false;
+    return 0;
+}
+
+static void sieve(unsigned char *a)
+{
+    unsigned int i, j; //, rooth = ((unsigned int)sqrtf(LIMIT)) >> 1;
+
+    memset(a, ISPRIME, HALF);
+    for (i = 1; i <= rooth; ++i) {
+        if (a[i] == ISPRIME) {
+            for (j = (i * i + i) << 1; j < HALF; j += (i << 1) + 1) {
+                a[j] = NOTPRIME;
+            }
+        }
+    }
+}
+
+static unsigned int count(unsigned char *a)
+{
+    unsigned int i, c = LIMIT > 2;
+
+    for (i = 1; i < HALF; ++i) {
+        if (a[i] == ISPRIME) {
+            ++c;
+        }
+    }
+    return c;
 }
 
 int main(void)
 {
-    unsigned int i, fac, nxt, dif, count, passes = 0;
+    unsigned char *a = (unsigned char *)malloc(HALF);
+    unsigned int passes = 0;
+    rooth = ((unsigned int)sqrtf(LIMIT)) >> 1;
+
     clock_t stop = clock() + FIVESEC;
-
     do {
-
-        // Init
-        for (i = 1; i < SIZE; ++i) {
-            isprime[i] = 1;
-        }
-
-        // Sieve
-        fac = 1;  // first factor to try is 3 = index 1 in the uneven numbers
-        nxt = 4;  // first multiple to scratch is factor * 3 = index 4 (skip factor * 2 is which is even)
-        dif = 3;  // step to next scratch is factor * 2 = index difference 3 (skip even multiples)
-        while (nxt < SIZE) {
-            if (isprime[fac]) {
-                for (i = nxt; i < SIZE; i += dif) {
-                    isprime[i] = 0;  // uneven multiple = not a prime
-                }
-            }
-            ++fac;
-            nxt += 3;
-            dif += 2;
-        }
-
-        // Count loops
+        sieve(a);
         ++passes;
-
     } while (clock() < stop);
 
-    // Count primes
-    count = (LIM >= 2);  // 2 is a prime
-    for (i = 1; i < SIZE; ++i) {
-        count += isprime[i];
-    }
-
-    // Result
-    printf("Passes: %u, Limit: %u, Count: %u, Valid: %s\n",
-        passes, LIM, count, isvalid(LIM, count) ? "yes" : "no");
+    unsigned int c = count(a);
+    printf("%u %u %u %s\n", passes, LIMIT, c, isvalid(LIMIT, c) ? "yes" : "no");
+    free(a);
     return 0;
 }
