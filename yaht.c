5bde141128174fd1360f7ecffb60733b3ec1898c
E. Dronkert <ednl@users.noreply.github.com>
2023-02-22 20:14:13 +0100

    csv output

--- yaht.c
+++ yaht.c
@@ -1,95 +1,96 @@
 // Simulate throwing with dice repeatedly to determine
-// chance of 'yahtzee' (5 the same) after 3 tries
+// chance of 'Yahtzee' (5 the same) after 3 (or N) tries.
 //
 // By E. Dronkert https://github.com/ednl
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <stdint.h>
 
-#define DICE  5UL
-#define MAJORITY ((DICE + 1UL) >> 1)
-#define FACES 6UL
-#define TRIES 200UL
-#define GAMES 10000000UL
-#define PERCT (GAMES / 100UL)
+#define DICE  5
+#define MAJORITY ((DICE + 1) >> 1)
+#define SIDES 6
+#define TRIES 200
+#define GAMES (100 * 1000 * 1000)
+#define PERCT (GAMES / 100)
 
 // Histograms
 // use index [1..max] inclusive for both
-static uint64_t vals[FACES + 1UL] = {0};
-static uint64_t yaht[TRIES + 1UL] = {0};
+static int cast[SIDES] = {0};
+static int hist[TRIES] = {0};
 
-// One unbiased die roll [1..FACES]
+// One unbiased die roll [0..SIDES - 1]
 // Ref. https://en.cppreference.com/w/c/numeric/random/rand
-static uint64_t roll(void)
+static int roll(void)
 {
-    return 1UL + random() / ((RAND_MAX + 1UL) / FACES);
+    return random() / ((RAND_MAX + 1UL) / SIDES);
 }
 
 int main(void)
 {
     srandom(time(NULL));
-    uint64_t games = GAMES, mintry = TRIES, maxtry = 0;
-
+    int games = GAMES;
     while (games--) {
         // What value occurs most, how many times
-        uint64_t try = 0, best = 0, count = 0;
+        int try = 0, best = 0, count = 0;
 
         // Get 'yahtzee' in any number of tries
         while (count < DICE) {
-            // Reset values histogram
-            for (uint64_t i = 1; i <= FACES; ++i)
-                if (i != best)
-                    vals[i] = 0;
+            ++try;
+
+            // Reset die count
+            if (count < MAJORITY) {
+                for (int i = 0; i < SIDES; ++i)
+                    cast[i] = 0;
+                if (count)
+                    cast[best] = count;
+            }
 
             // Roll remaining dice
-            for (uint64_t i = count; i < DICE; ++i)
-                ++vals[roll()];
+            for (int i = count; i < DICE; ++i)
+                ++cast[roll()];
 
             // Find most common value
             if (count < MAJORITY) {
-                for (uint64_t i = 1; i <= FACES; ++i)
-                    if (vals[i] > count) {
-                        count = vals[i];
-                        best = i;
-                    }
+                for (int i = 0; i < SIDES; ++i)
+                    if (cast[i] > count)
+                        count = cast[(best = i)];
             } else
-                count = vals[best];
-
-            ++try;
+                count = cast[best];
         }
-
-        if (try < mintry)
-            mintry = try;
-        if (try > maxtry)
-            maxtry = try;
-        if (try <= TRIES)
-            ++yaht[try];
+        if (try < TRIES)
+            ++hist[try];
     }
 
-    uint64_t maxval = 0, mode = 0;
+    int mintry = 0, maxtry = TRIES - 1;
+    while (!hist[mintry])
+        ++mintry;
+    while (!hist[maxtry])
+        --maxtry;
+
+    printf("Tries,Count,Partial,Cumulative\n");
+    int maxval = 0, mode = 0;
     double cumul = 0, median = 0, mean = 0;
-    for (uint64_t i = mintry; i <= maxtry && i <= TRIES; ++i) {
-        if (yaht[i] > maxval) {
-            maxval = yaht[i];
-            mode = i;
-        }
-        if (cumul < 0.5) {
-            double prev = cumul;
-            cumul += (double)yaht[i] / GAMES;
-            if (cumul >= 0.5)
-                median = i - 1 + (0.5 - prev) / (cumul - prev);
-        }
-        mean += (double)(i * yaht[i]) / GAMES;
+    for (int i = mintry; i <= maxtry; ++i) {
+        if (hist[i] > maxval)
+            maxval = hist[(mode = i)];
+        double prev = cumul, part = (double)hist[i] / GAMES;
+        cumul += part;
+        printf("%d,%d,%.8f,%.8f\n", i, hist[i], part, cumul);
+        if (prev < 0.5 && cumul >= 0.5)
+            median = i - 1 + (0.5 - prev) / (cumul - prev);
+        mean += part * i;
     }
+    printf("\n");
 
-    printf("N_min    = %3llu\n", mintry);
-    printf("N_max    = %3llu\n", maxtry);
-    printf("N_mode   = %3llu\n", mode);
+    printf("N_min    = %3d\n", mintry);
+    printf("N_max    = %3d\n", maxtry);
+    printf("N_mode   = %3d\n", mode);
     printf("N_median = %6.2f (1st ord appr)\n", median);
     printf("N_mean   = %6.2f\n\n", mean);
-    printf("Chance of Yahtzee in 1: %.3f%%\n", (double)yaht[1] / PERCT);
-    printf("Chance of Yahtzee in 3: %.2f%%\n", (double)(yaht[1] + yaht[2] + yaht[3]) / PERCT);
+    printf("Chance of Yahtzee in 1: %.4f%%\n", (double)hist[1] / PERCT);
+    printf("Chance of Yahtzee in 3: %.3f%%\n", (double)(hist[1] + hist[2] + hist[3]) / PERCT);
 
     return 0;
 }
