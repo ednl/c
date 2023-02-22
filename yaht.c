96caa7f2736bcd78cd7cbf06efd31ae79b1143ea
E. Dronkert <ednl@users.noreply.github.com>
2023-02-22 07:52:05 +0100

    re-eval

--- yaht.c
+++ yaht.c
@@ -7,56 +7,52 @@
 #include <stdlib.h>
 #include <time.h>
 
-#define DICE  5
-#define FACES 6
-#define TRIES 3
-#define GAMES 10000000
+#define DICE  5UL
+#define FACES 6UL
+#define TRIES 3UL
+#define GAMES 10000000UL
 
 // Histogram
-static int hist[FACES + 1];  // use index [1..FACES]
+static uint64_t hist[FACES + 1UL];  // use index [1..FACES]
 
 // One unbiased die roll [1..FACES]
 // Ref. https://en.cppreference.com/w/c/numeric/random/rand
-static int roll(void)
+static uint64_t roll(void)
 {
-    return 1 + rand() / ((RAND_MAX + 1U) / FACES);
+    return 1UL + random() / ((RAND_MAX + 1UL) / FACES);
 }
 
 int main(void)
 {
-    srand(time(NULL));
-    int yahtzee = 0, games = GAMES;
+    srandom(time(NULL));
+    uint64_t yahtzee = 0, games = GAMES;
 
     while (games--) {
-        // Reset histogram
-        for (int i = 1; i <= FACES; ++i)
-            hist[i] = 0;
-
         // What value occurs most, how many times
-        int best = 0, count = 0;
+        uint64_t best = 0, count = 0;
 
         // Try to get 'yahtzee' in a number of tries
-        for (int j = 0; j < TRIES && count < DICE; ++j) {
+        for (uint64_t j = 0; j < TRIES && count < DICE; ++j) {
+            // Reset histogram
+            for (uint64_t i = 1; i <= FACES; ++i)
+                if (i != best)
+                    hist[i] = 0;
+
             // Roll remaining dice
-            for (int i = count; i < DICE; ++i)
+            for (uint64_t i = count; i < DICE; ++i)
                 hist[roll()]++;
 
-            if (!best) {
-                // Find most common value after first try
-                for (int i = 1; i <= FACES; ++i)
-                    if (hist[i] > count) {
-                        count = hist[i];
-                        best = i;
-                    }
-            } else
-                count = hist[best];
+            // Find most common value
+            for (uint64_t i = 1; i <= FACES; ++i)
+                if (hist[i] > count) {
+                    count = hist[i];
+                    best = i;
+                }
         }
-
         // Test for yahtzee
         if (count == DICE)
             yahtzee++;
     }
-
-    printf("Chance of yahtzee: %.2f%%\n", (double)yahtzee / GAMES * 100);
+    printf("Chance of yahtzee: %.3f%%\n", (double)yahtzee / GAMES * 100UL);
     return 0;
 }
