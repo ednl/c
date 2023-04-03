2201684d97e95f4a046e5af76f8eca7dcf8e464d
E. Dronkert <ednl@users.noreply.github.com>
2023-04-03 13:39:16 +0200

    more checks

--- vm-codeobserver.c
+++ vm-codeobserver.c
@@ -3,69 +3,240 @@
 // Puzzle designed by https://www.reddit.com/user/codeobserver for https://codeguppy.com
 // Solution by E. Dronkert https://github.com/ednl
 
-#include <stdio.h>   // printf
-#include <stdlib.h>  // malloc, free
-
-// VM stack size (max size used for this puzzle: 2)
-#define STACKSZ 16
+#include <stdio.h>    // printf
+#include <stdlib.h>   // malloc, free
+#include <stdbool.h>  // bool
 
 // Program code
 static const char *input = "11,0,10,42,6,255,30,0,11,0,0,11,1,1,11,3,1,60,1,10,2,0,20, 2,1,60,2,10,0,1,10,1,2,11,2,1,20,3,2,31,2,30,2,41,3,2,19,31,0,50";
 
-// Global vars (auto init to 0)
-static int *mem;
-static int stack[STACKSZ], sp;
-static int R[4];
+typedef struct {
+    int *mem, *stk, *reg;
+    int result;
+    size_t pc, sp, tick, tock;
+    size_t memsize, progsize, stksize, regsize;
+    bool halted, silent;
+} VirtualMachine;
 
-static int run(void)
+static size_t fieldcount(const char * const csvline)
 {
-    int ip = 0, clock = 0;
-    while (1) {
-        ++clock;
-        switch (mem[ip++]) {
-            case  10: R[mem[ip]]  = R[mem[ip + 1]]; ip += 2; break;  // MOV   rdst rsrc
-            case  11: R[mem[ip]]  =   mem[ip + 1];  ip += 2; break;  // MOV   rdst val
-            case  20: R[mem[ip]] += R[mem[ip + 1]]; ip += 2; break;  // ADD   rdst rsrc
-            case  21: R[mem[ip]] -= R[mem[ip + 1]]; ip += 2; break;  // SUB   rdst rsrc
-            case  30: stack[sp++]  = R[mem[ip++]];           break;  // PUSH  rsrc
-            case  31: R[mem[ip++]] = stack[--sp];            break;  // POP   rdst
-            case  40: ip = mem[ip];                          break;  // JP    addr
-            case  41: ip = R[mem[ip]] < R[mem[ip + 1]] ? mem[ip + 2] : ip + 3; break;  // JL reg1 reg2 addr
-            case  42: stack[sp++] = ip + 1; ip = mem[ip];    break;  // CALL  addr (push ret-addr)
-            case  50: ip = stack[--sp];                      break;  // RET   (pop ret-addr)
-            case  60: printf("%d\n", R[mem[ip++]]);          break;  // PRINT reg
-            case 255: return clock;                                  // HALT
-        }
-    }
-}
-
-int main(void)
-{
-    // Count values in CSV data
-    size_t codesize = 1;
-    const char *c = input;
+    if (!csvline || *csvline == '\0')
+        return 0;
+    size_t fields = 1;
+    const char *c = csvline;
     while (*c)
         if (*c++ == ',')
-            ++codesize;
+            ++fields;
+    return fields;
+}
 
-    // Allocate memory
-    mem = malloc(codesize * sizeof *mem);
-    if (!mem)
-        exit(1);
+static void* del_vm(VirtualMachine *vm)
+{
+    free(vm->mem);
+    free(vm->stk);
+    free(vm->reg);
+    free(vm);
+    vm = NULL;
+    return NULL;
+}
 
-    // Parse CSV data to memory
-    c = input;
-    for (int i = 0; i < codesize; ++i) {
-        sscanf(c, "%d", &mem[i]);
+static VirtualMachine* new_vm(size_t memsize, size_t stksize, size_t regsize)
+{
+    if (!memsize)
+        return NULL;
+    VirtualMachine *vm = calloc(1, sizeof *vm);  // resets all vars
+    if (!vm)
+        return NULL;
+    if (memsize) {
+        vm->mem = calloc(memsize, sizeof *(vm->mem));
+        if (!vm->mem)
+            return del_vm(vm);
+        vm->memsize = memsize;
+    }
+    if (stksize) {
+        vm->stk = calloc(stksize, sizeof *(vm->stk));
+        if (!vm->mem)
+            return del_vm(vm);
+        vm->stksize = stksize;
+    }
+    if (regsize) {
+        vm->reg = calloc(regsize, sizeof *(vm->reg));
+        if (!vm->mem)
+            return del_vm(vm);
+        vm->regsize = regsize;
+    }
+    return vm;
+}
+
+static bool saveprog(VirtualMachine *vm, const char * const csvline)
+{
+    if (!vm || !vm->memsize || !csvline || !*csvline)
+        return false;
+    vm->progsize = 0;
+    const char *c = csvline;
+    while (*c && vm->progsize < vm->memsize) {
+        int val;
+        if (sscanf(c, "%d", &val) != 1)
+            return false;
+        vm->mem[vm->progsize++] = val;
         while (*c && *c != ',')
             ++c;
         if (*c == ',')
             ++c;
     }
+    return true;
+}
 
-    // Run & clean up
-    // Executes 100 instructions, prints first 10 numbers of Fibonacci sequence
-    printf("clock=%d\n", run());
-    free(mem);
+static int tick(VirtualMachine *vm)
+{
+    if (!vm)
+        return -1;  // TODO: define error codes
+    if (vm->halted)
+        return 0;
+    if (vm->pc >= vm->progsize) {
+        vm->halted = true;
+        return -2;
+    }
+    // Get instruction
+    vm->tick++;
+    int opcode = vm->mem[vm->pc++];
+    // Get parameters
+    int par[3] = {0}, parcount = 0, i = 0;
+    switch (opcode) {
+        case  41: // JL   (3 params)
+            parcount = 3;
+            if (vm->pc < vm->progsize)
+                par[i++] = vm->mem[vm->pc++];
+        case  10: // MOVR (2 params)
+        case  11: // MOVV
+        case  20: // ADD
+        case  21: // SUB
+            if (!parcount)
+                parcount = 2;
+            if (vm->pc < vm->progsize)
+                par[i++] = vm->mem[vm->pc++];
+        case  30: // PUSH (1 param)
+        case  31: // POP
+        case  40: // JP
+        case  42: // CALL
+        case  60: // PRINT
+            if (!parcount)
+                parcount = 1;
+            if (vm->pc < vm->progsize)
+                par[i++] = vm->mem[vm->pc++];
+        case   0: // NOP  (0 params)
+        case  50: // RET
+        case 255: // HALT
+            break;
+        default:  // invalid opcode
+            vm->halted = true;
+            return -3;
+    }
+    // Correct number of parameters available?
+    if (i != parcount) {
+        vm->halted = true;
+        return -4;
+    }
+    // TODO: validate parameters according to addressing mode
+    // Execute instruction
+    switch (opcode) {
+        case 10: // MOVR Rdst Rsrc
+            vm->reg[par[0]] = vm->reg[par[1]];
+            break;
+        case 11: // MOVV Rdst val
+            vm->reg[par[0]] = par[1];
+            break;
+        case 20: // ADD Rdst Rsrc
+            vm->reg[par[0]] += vm->reg[par[1]];
+            break;
+        case 21: // SUB Rdst Rsrc
+            vm->reg[par[0]] -= vm->reg[par[1]];
+            break;
+        case 30: // PUSH Rsrc
+            if (vm->sp >= vm->stksize) {
+                vm->halted = true;
+                return -5;
+            }
+            vm->stk[vm->sp++] = vm->reg[par[0]];
+            break;
+        case 31: // POP Rdst
+            if (vm->sp == 0 || vm->sp > vm->stksize) {
+                vm->halted = true;
+                return -6;
+            }
+            vm->reg[par[0]] = vm->stk[--vm->sp];
+            break;
+        case 40: // JP addr
+            if (par[0] >= vm->progsize) {
+                vm->halted = true;
+                return -7;
+            }
+            vm->pc = par[0];
+            break;
+        case 41: // JL Rx Ry addr
+            if (vm->reg[par[0]] < vm->reg[par[1]]) {
+                if (par[2] >= vm->progsize) {
+                    vm->halted = true;
+                    return -8;
+                }
+                vm->pc = par[2];
+            }
+            break;
+        case 42: // CALL addr (+ push ret-addr)
+            if (vm->sp >= vm->stksize) {
+                vm->halted = true;
+                return -9;
+            }
+            if (par[0] >= vm->progsize) {
+                vm->halted = true;
+                return -10;
+            }
+            vm->stk[vm->sp++] = vm->pc;
+            vm->pc = par[0];
+            break;
+        case 50: // RET (+ pop ret-addr)
+            if (vm->sp == 0 || vm->sp > vm->stksize) {
+                vm->halted = true;
+                return -11;
+            }
+            int addr = vm->stk[--vm->sp];
+            if (addr >= vm->progsize) {
+                vm->halted = true;
+                return -12;
+            }
+            vm->pc = addr;
+            break;
+        case 60: // PRINT Rsrc
+            vm->result = vm->reg[par[0]];
+            if (!vm->silent)
+                printf("%d\n", vm->result);
+            break;
+        case 255: // HALT
+            vm->halted = true;
+            break;
+        default: // invalid opcode
+            vm->halted = true;
+            return -3;
+    }
+    vm->tock++;
+    return 0;
+}
+
+static int run(VirtualMachine *vm)
+{
+    if (!vm)
+        return -1;
+    int exitcode = 0;
+    while (!vm->halted)
+        exitcode = tick(vm);
+    return exitcode;
+}
+
+int main(void)
+{
+    int proglen = fieldcount(input);
+    VirtualMachine *vm = new_vm(proglen, 16, 4);  // stack size used by puzzle = 2
+    saveprog(vm, input);
+    run(vm);  // prints first 10 Fibonacci numbers
     return 0;
 }
