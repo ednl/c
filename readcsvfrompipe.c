718d1dcdd668c76b687d21b63d2d4ad7024884d2
E. Dronkert <ednl@users.noreply.github.com>
2023-04-11 20:14:33 +0200

    variable names

--- readcsvfrompipe.c
+++ readcsvfrompipe.c
@@ -3,26 +3,26 @@
 #include <unistd.h>   // isatty, fileno
 #include <stdbool.h>  // bool
 
-static bool grow(int ** array, size_t * const len)
+static bool grow(int ** a, size_t * const size)
 {
     static const size_t inc = 32;
-    if (!array || !len)
+    if (!a || !size)
         return false;
-    size_t newlen = *len + inc;
-    void *a = realloc(*array, newlen * sizeof **array);
-    if (!a)
+    size_t newlen = *size + inc;
+    void *p = realloc(*a, newlen * sizeof **a);
+    if (!p)
         return false;
-    *array = a;
-    *len = newlen;
+    *a = p;
+    *size = newlen;
     return true;
 }
 
-static size_t csv2int(int **array, size_t *len, const char * const line)
+static size_t csv2int(int ** a, size_t * size, const char * const line)
 {
-    if (!array || !line || !*line)
+    if (!a || !line || !*line)
         return 0;
-    if (!*array && *len)
-        *len = 0;
+    if (!*a && *size)
+        *size = 0;
     const char *c = line;
     while (*c && *c != ',' && *c != ';')
         ++c;
@@ -30,12 +30,12 @@ static size_t csv2int(int **array, size_t *len, const char * const line)
     c = line;
     size_t i = 0;
     while (*c) {
-        if (i >= *len && !grow(array, len))
+        if (i >= *size && !grow(a, size))
             return i;
         int val;
         if (sscanf(c, "%d", &val) != 1)
             val = 0;
-        (*array)[i++] = val;
+        (*a)[i++] = val;
         while (*c && *c != delim)
             ++c;
         if (*c == delim)
@@ -47,23 +47,23 @@ static size_t csv2int(int **array, size_t *len, const char * const line)
 int main(int argc, char *argv[])
 {
     int *a = NULL;
-    size_t len = 0, last = 0;
+    size_t size = 0, range = 0;
 
     if (isatty(fileno(stdin))) {
         // No pipe or redirect, try command line argument
         if (argc > 1)
-            last = csv2int(&a, &len, argv[1]);
+            range = csv2int(&a, &size, argv[1]);
     } else {
         // Input is pipe or redirect to stdin of this program
         char *buf = NULL;
         size_t bufsz;
         if (getline(&buf, &bufsz, stdin) > 0)
-            last = csv2int(&a, &len, buf);
+            range = csv2int(&a, &size, buf);
         free(buf);
     }
 
     if (a) {
-        for (size_t i = 0; i < last; ++i)
+        for (size_t i = 0; i < range; ++i)
             printf("%zu: %d\n", i, a[i]);
         free(a);
     }
