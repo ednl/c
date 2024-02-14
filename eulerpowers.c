b58381d8893fd46f1274d4a19eb897df889e9bb9
E. Dronkert <ednl@users.noreply.github.com>
2024-02-14 14:26:24 +0100

    euler sum of powers

--- eulerpowers.c
+++ eulerpowers.c
@@ -1,61 +1,47 @@
 // https://en.wikipedia.org/wiki/Euler%27s_sum_of_powers_conjecture
+// Counterexample: find N Kth powers whose sum is equal to a Kth power where N<K.
+// For N=4, K=5 should find: 144^5 = 27^5 + 84^5 + 110^5 + 133^5
+// Code slightly optimised from https://www.reddit.com/r/math/comments/1apnbf5/has_there_even_been_a_famous_and_longstanding/kq92guo/
 
-#include <stdio.h>
-#include <stdint.h>
+#include <stdio.h>   // printf
+#include <stdint.h>  // int64_t
+#include <math.h>    // exp, log
 
-#define N 256u
-#define K 5u
-#define LEN 5u  // 4 or 5
+static int64_t pow4(int64_t x)
+{
+    x *= x;
+    x *= x;
+    return x;
+}
 
-static uint64_t p[N] = {0, 1};
+static int64_t pow5(int64_t x)
+{
+    return pow4(x) * x;
+}
 
 int main(void)
 {
-    for (uint64_t i = 2; i < N; ++i) {
-        uint64_t j = i;
-        for (unsigned k = 1; k < K; ++k)
-            j *= i;
-        p[i] = j;
-    }
-
-    for (unsigned i = 1; i < N - 5; ++i)
-        for (unsigned j = i + 1; j < N - 4; ++j) {
-            uint64_t s2 = p[i] + p[j];
-            if (s2 > p[N - 1]) break;
-            for (unsigned k = j + 1; k < N - 3; ++k) {
-                uint64_t s3 = s2 + p[k];
-                if (s3 > p[N - 1]) break;
-                for (unsigned l = k + 1; l < N - 2; ++l) {
-                    uint64_t s4 = s3 + p[l];
-                    if (s4 > p[N - 1]) break;
-                #if LEN == 4
-                    for (unsigned m = l + 1; m < N; ++m) {
-                        if (p[m] < s4) continue;
-                        if (p[m] > s4) break;
-                        printf("%u^5 + %u^5 + %u^5 + %u^5 = %u^5\n", i, j, k, l, m);
-                        // 27^5 + 84^5 + 110^5 + 133^5 = 144^5
+    for (int64_t n = 5;; ++n) {
+        const int64_t target = pow5(n);
+        for (int64_t a = 1;; ++a) {
+            const int64_t a5 = pow5(a);
+            const int64_t res_a = target - a5;
+            if (res_a <= a5) break;
+            for (int64_t b = a + 1;; ++b) {
+                const int64_t b5 = pow5(b);
+                const int64_t res_b = res_a - b5;
+                if (res_b <= b5) break;
+                for (int64_t c = b + 1;; ++c) {
+                    const int64_t c5 = pow5(c);
+                    const int64_t res_c = res_b - c5;
+                    if (res_c <= c5) break;
+                    const int64_t d = (int64_t)(round(exp(0.2 * log(res_c))));
+                    if (pow5(d) == res_c) {
+                        printf("%lld^5 = %lld^5 + %lld^5 + %lld^5 + %lld^5\n", n, a, b, c, d);
+                        return 0;
                     }
-                #elif LEN == 5
-                    for (unsigned m = l + 1; m < N - 1; ++m) {
-                        uint64_t s5 = s4 + p[m];
-                        if (s5 > p[N - 1]) break;
-                        for (unsigned n = m + 1; n < N; ++n) {
-                            if (p[n] < s5) continue;
-                            if (p[n] > s5) break;
-                            printf("%u^5 + %u^5 + %u^5 + %u^5 + %u^5 = %u^5\n", i, j, k, l, m, n);
-                            // 7^5 + 43^5 + 57^5 + 80^5 + 100^5 = 107^5
-                            // 14^5 + 86^5 + 114^5 + 160^5 + 200^5 = 214^5 (common factor 2)
-                            // 19^5 + 43^5 + 46^5 + 47^5 + 67^5 = 72^5
-                            // 21^5 + 23^5 + 37^5 + 79^5 + 84^5 = 94^5
-                            // 38^5 + 86^5 + 92^5 + 94^5 + 134^5 = 144^5 (common factor 2)
-                            // 42^5 + 46^5 + 74^5 + 158^5 + 168^5 = 188^5 (common factor 2)
-                            // 57^5 + 129^5 + 138^5 + 141^5 + 201^5 = 216^5 (common factor 3)
-                        }
-                    }
-                #endif
                 }
             }
         }
-
-    return 0;
+    }
 }
