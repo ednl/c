e7cb1e97cdab2ecfe43b8123b16f71a8d8c639ae
E. Dronkert <ednl@users.noreply.github.com>
2024-02-14 14:49:52 +0100

    caching

--- eulerpowers.c
+++ eulerpowers.c
@@ -8,37 +8,39 @@
 #include <math.h>    // exp, log
 // #include "startstoptimer.h"
 
-static int64_t pow4(int64_t x)
+static int64_t p[4096];
+
+static int64_t fourth(int64_t x)
 {
     x *= x;
     x *= x;
     return x;
 }
 
-static int64_t pow5(int64_t x)
+static int64_t fifth(int64_t x)
 {
-    return pow4(x) * x;
+    return p[x] ? p[x] : (p[x] = fourth(x) * x);
 }
 
 int main(void)
 {
     // starttimer();
     for (int64_t n = 5;; ++n) {
-        const int64_t target = pow5(n);
+        const int64_t target = fifth(n);
         for (int64_t a = 1;; ++a) {
-            const int64_t a5 = pow5(a);
+            const int64_t a5 = fifth(a);
             const int64_t res_a = target - a5;
             if (res_a <= a5) break;
             for (int64_t b = a + 1;; ++b) {
-                const int64_t b5 = pow5(b);
+                const int64_t b5 = fifth(b);
                 const int64_t res_b = res_a - b5;
                 if (res_b <= b5) break;
                 for (int64_t c = b + 1;; ++c) {
-                    const int64_t c5 = pow5(c);
+                    const int64_t c5 = fifth(c);
                     const int64_t res_c = res_b - c5;
                     if (res_c <= c5) break;
                     const int64_t d = (int64_t)(round(exp(0.2 * log(res_c))));
-                    if (pow5(d) == res_c) {
+                    if (fifth(d) == res_c) {
                         printf("%lld^5 = %lld^5 + %lld^5 + %lld^5 + %lld^5\n", n, a, b, c, d);
                         // printf("Time: %.0f ms\n", stoptimer_ms());
                         return 0;
