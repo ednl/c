0f2ab09d103df6f1eefd0675f827ddd07957b311
E. Dronkert <ednl@users.noreply.github.com>
2024-03-17 05:23:26 +0100

    piday

--- reddit-aoc-piday.c
+++ reddit-aoc-piday.c
@@ -1,15 +1,16 @@
 #include <stdio.h>
 #include <string.h>  // strstr
 
-#define L ('z' - 'a' + 1)
+#define ALPH ('z' - 'a' + 1)  // 26 letters
+#define CASE 32  // lowercase = uppercase | 32
 
 static const char pi[] = "314159265358979323846264338327950288";
 static const char* num[] = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten"};
 static char msg[] = "Wii kxtszof ova fsegyrpm d lnsrjkujvq roj! Kdaxii svw vnwhj pvugho buynkx tn vwh-gsvw ruzqia. Mrq'x kxtmjw bx fhlhlujw cjoq! Hmg tyhfa gx dwd fdqu bsm osynbn oulfrex, kahs con vjpmd qtjv bx whwxssp cti hmulkudui f Jgusd Yp Gdz!";
 
-static inline char decode(const char c, const char base, const int index)
+static inline char shift(const char letter, const char base, const int index)
 {
-    return base + (L + c - base - pi[index & 15] + '0') % L;
+    return base + (ALPH + letter - base - (pi[index & 15] & 15)) % ALPH;
 }
 
 int main(void)
@@ -18,9 +19,9 @@ int main(void)
     int i = 0, count = 0;
     for (char* s = msg; *s; ++s, ++i)
         if (*s >= 'a' && *s <= 'z')
-            letters[count++] = *s = decode(*s, 'a', i);
+            letters[count++] = *s = shift(*s, 'a', i);
         else if ((*s >= 'A' && *s <= 'Z'))
-            letters[count++] = (*s = decode(*s, 'A', i)) | 32;
+            letters[count++] = (*s = shift(*s, 'A', i)) | CASE;
     printf("Part 1: %s\n", msg);
 
     int prod = 1;
