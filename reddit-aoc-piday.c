d0f1232bad1e7857dae7df3a4fef1340a7e18436
E. Dronkert <ednl@users.noreply.github.com>
2024-03-20 14:23:16 +0100

    more clarity

--- reddit-aoc-piday.c
+++ reddit-aoc-piday.c
@@ -14,34 +14,34 @@
 
 #define ALPH ('z' - 'a' + 1)  // 26 letters
 #define CASE 32  // lowercase = uppercase | 32
+#define BASE 96  // bitmask to select base of alphabet: 'A' - 1 or 'a' - 1
+#define NIBBLE ((1 << 4) - 1)  // 4 bits
 
 static const char pi[] = "314159265358979323846264338327950288";  // copied from math.h::M_PI
 static const char* num[] = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten"};
 static char msg[] = "Wii kxtszof ova fsegyrpm d lnsrjkujvq roj! Kdaxii svw vnwhj pvugho buynkx tn vwh-gsvw ruzqia. Mrq'x kxtmjw bx fhlhlujw cjoq! Hmg tyhfa gx dwd fdqu bsm osynbn oulfrex, kahs con vjpmd qtjv bx whwxssp cti hmulkudui f Jgusd Yp Gdz!";
 
-static inline char shift(const char letter, const char base, const int index)
-{
-    const char amount = pi[index & 15] & 15;  // index 0..15 repeating, convert '0'..'9' to 0..9
-    return base + (ALPH + letter - base - amount) % ALPH;  // subtract shift amount to decode
-}
-
 int main(void)
 {
     // Decode message and save lowercased letters to continuous string.
-    char letters[256] = {0};
+    char letters[256] = {0};  // large enough to contain all letters of msg
     int i = 0, count = 0;
     for (char* s = msg; *s; ++s, ++i)
-        if (*s >= 'A')
-            letters[count++] = (*s = shift(*s, (*s & 96) | 1, i)) | CASE;
-    printf("Part 1: %s\n", msg);
+        if (*s >= 'A') {
+            const char base = (*s & BASE) | 1;  // 'A' or 'a' (assume ASCII)
+            const char shift = pi[i & NIBBLE] & NIBBLE;  // index 0..15 repeating, convert '0'..'9' to 0..9
+            *s = base + (*s - base + ALPH - shift) % ALPH;  // decode by subtracting shift amount
+            letters[count++] = *s | CASE;  // save lowercase letters
+        }
+    printf("Part 1: \"%s\"\n", msg);  // The formula for crafting a delightful pie! Cutoff our three golden apples of one-four pounds. Don't forget to weighten well! Add sugar as you want and invite friends, even the silly ones to network and celebrate a Happy Pi Day!
 
     // Take product of all number words in message.
     int prod = 1;
-    for (int n = 2; n <= 10; ++n) {  // "zero" added for alignment, skip "one" (product identity)
+    for (int n = 2; n <= 10; ++n) {  // "zero" only for alignment, skip product identity "one"
         const size_t len = strlen(num[n]);
-        for (const char* s = letters; (s = strstr(s, num[n])); s += len)  // NULL = not found
+        for (const char* s = letters; (s = strstr(s, num[n])); s += len)  // strstr = NULL when not found
             prod *= n;
     }
-    printf("Part 2: %d\n", prod);
+    printf("Part 2: %d\n", prod);  // 53760
     return 0;
 }
