ed4dd4c69af00e6c0d09756bd584ec25bccf6dfc
E. Dronkert <ednl@users.noreply.github.com>
2024-05-23 02:43:15 +0200

    multi-threaded!

--- tetrasum.c
+++ tetrasum.c
@@ -1,19 +1,19 @@
 /******************************************************************************
  * Brady Haran of Numberphile asked: which 5 tetrahedral numbers sum to 343867?
- * This is my solution that finds 277 different combinations of 5 distinct
- * tetrahedral numbers. I implemented Donald Knuth's algorithm to find all
- * combinations of k elements from a set of n, from The Art of Computer
- * Programming - Volume 4A - Combinatorial Algorithms, §7.2.1.3, algorithm T.
+ * This is my multi-threaded solution that finds 322 different combinations of
+ * 5 tetrahedral numbers (277 all unique, 45 with duplicates).
  *
- * Compile: clang -std=gnu17 -Ofast -o tetrasum tetrasum.c combinations.c
- * Run: ./tetrasum 343867 5     : show tetrahedral numbers and no. of solutions
- *  or: ./tetrasum -v 343867 5  : show everything (verbose)
- *  or: ./tetrasum -q 343867 5  : only show no. of solutions (quiet)
+ * Compile: clang -std=gnu17 -Ofast -o tetrasum tetrasum.c
+ * Run: ./tetrasum 343867     : show sums with up to 4 terms, and summary
+ *  or: ./tetrasum -v 343867  : show everything (verbose)
+ *  or: ./tetrasum -q 343867  : only show no. of solutions (quiet)
  *
  * E. Dronkert
  * Utrecht, Netherlands
- * 2024-05-20
  * https://github.com/ednl/c/blob/master/tetrasum.c
+ *
+ * 2024-05-20 v1  First try with unique combinations.
+ * 2024-05-23 v2  Now as requested with duplicates, and threads.
  *
  * Numberphile video is out now: https://www.youtube.com/watch?v=CK3_zarXkw0
  * Turns out this was in reference to one of Pollock's conjectures, see
@@ -21,131 +21,208 @@
  * to be the last (largest) sum that needs five tetrahedral numbers. But they
  * don't have to be distinct! E.g. 17 is the first (smallest) that needs more
  * than four: 17 = 10 + 4 + 1 + 1 + 1.
- *
- * So unfortunately this program does not answer Brady's question because it
- * misses solutions with duplicate tetrahedral numbers.
  ******************************************************************************/
 
 #include <stdio.h>
-#include <stdlib.h>  // atoll, malloc, free
-#include <limits.h>  // INT_MAX
-#include <math.h>    // cbrt, ceil
-#include <string.h>  // strcmp
+#include <stdlib.h>    // malloc, free
+#include <math.h>      // cbrt, floor
+#include <stdint.h>    // uint32_t, UINT32_MAX
+#include <inttypes.h>  // PRIu32
+#include <string.h>    // strcmp
+#if __APPLE__
+    #include <sys/sysctl.h>  // sysctlbyname
+    #include <pthread.h>     // pthread_create, pthread_join
+#elif __linux__
+    #define _GNU_SOURCE
+    #include <sched.h>  // sched_getaffinity
+    // TODO: use threads on Linux
+#endif
 
-// My own "n choose k" combinations function.
-#include "combinations.h"
+#define MAXTHREADS 32
+static uint32_t threads, target, N, total, dup, part[5];
+static uint32_t *tetra;
+static uint8_t verbose = 1;
 
-// Avoid overflow when determining tetrahedral numbers to choose from.
-#define TARGET_MAX (INT_MAX / 6)
-
-// Arbitrary limit, but n!/(k!(n-k)!) grows fast.
-#define PARTS_MAX 128
-
-// Calculate the n'th tetrahedral number.
-static int tetracalc(const int n)
+// How many CPU cores available to this program?
+static int coresavail(const int min, const int max)
 {
-    return ((n * (n + 1)) >> 1) * (n + 2) / 3;  // avoid early overflow by dividing in 2 steps
+    int n = 0;
+#if __APPLE__
+    size_t size = sizeof n;
+    sysctlbyname("hw.activecpu", &n, &size, NULL, 0);
+#elif __linux__
+    cpu_set_t set;
+    CPU_ZERO(&set);
+    sched_getaffinity(0, sizeof set, &set);
+    n = CPU_COUNT(&set);
+#endif
+    return n < min ? min : (n > max ? max : n);
+}
+
+// Parallel execution in separate threads.
+static void *loop(void *arg)
+{
+    const uint32_t start = *(uint32_t *)arg;
+
+    for (uint32_t i = start; i < N; i += threads) {
+        uint32_t a = tetra[i];
+        if (a == target) {
+            ++total;
+            ++part[0];
+            if (verbose)
+                printf("%"PRIu32"\n", tetra[i]);
+        }
+
+        for (uint32_t j = i; j < N; ++j) {
+            uint32_t b = a + tetra[j];
+            if (b < a || b > target)
+                break;
+            if (b == target) {
+                ++total;
+                ++part[1];
+                dup += i == j;
+                if (verbose)
+                    printf("%"PRIu32" + %"PRIu32"\n", tetra[i], tetra[j]);
+                break;
+            }
+
+            for (uint32_t k = j; k < N; ++k) {
+                uint32_t c = b + tetra[k];
+                if (c < b || c > target)
+                    break;
+                if (c == target) {
+                    ++total;
+                    ++part[2];
+                    dup += i == j || j == k;
+                    if (verbose)
+                        printf("%"PRIu32" + %"PRIu32" + %"PRIu32"\n", tetra[i], tetra[j], tetra[k]);
+                    break;
+                }
+
+                for (uint32_t l = k; l < N; ++l) {
+                    uint32_t d = c + tetra[l];
+                    if (d < c || d > target)
+                        break;
+                    if (d == target) {
+                        ++total;
+                        ++part[3];
+                        dup += i == j || j == k || k == l;
+                        if (verbose)
+                            printf("%"PRIu32" + %"PRIu32" + %"PRIu32" + %"PRIu32"\n", tetra[i], tetra[j], tetra[k], tetra[l]);
+                        break;
+                    }
+
+                    // Try to find fifth term.
+                    uint32_t res = target - d;  // residue
+                    if (res < tetra[l] || res > tetra[N - 1])
+                        continue;
+                    if (res == tetra[l] || res == tetra[N - 1]) {
+                        uint32_t m = res == tetra[l] ? l : N - 1;
+                        ++total;
+                        ++part[4];
+                        dup += i == j || j == k || k == l || l == m;
+                        if (verbose == 2)
+                            printf("%"PRIu32" + %"PRIu32" + %"PRIu32" + %"PRIu32" + %"PRIu32"\n", tetra[i], tetra[j], tetra[k], tetra[l], tetra[m]);
+                        continue;
+                    }
+
+                    // No edge case, now do binary search.
+                    uint32_t min = l, max = N - 1, m;
+                    while (min < max) {
+                        m = ((min + 1) >> 1) + (max >> 1);
+                        if (m == min || m == max) break;
+                        if      (tetra[m] < res) min = m;
+                        else if (tetra[m] > res) max = m;
+                        else {
+                            ++total;
+                            ++part[4];
+                            dup += i == j || j == k || k == l || l == m;
+                            if (verbose == 2)
+                                printf("%"PRIu32" + %"PRIu32" + %"PRIu32" + %"PRIu32" + %"PRIu32"\n", tetra[i], tetra[j], tetra[k], tetra[l], tetra[m]);
+                            break;
+                        };
+                    }
+                }
+            }
+        }
+    }
+    return NULL;
 }
 
 int main(int argc, char *argv[])
 {
-    // Verbosity level.
-    int argoffset = 0;
-    int verbose = 1;
+    const int cores = coresavail(1, MAXTHREADS);
+    pthread_t tid[MAXTHREADS];
+    uint32_t arg[MAXTHREADS];
+
+    // Pollock's conjecture: all numbers are sums of at
+    // most 5 tetrahedral numbers (duplicates allowed).
+    // This is probably the largest number that requires 5.
+    target = 343867;
+
+    // Command line arguments.
     if (argc > 1) {
+        int argofs = 0;
+        // Verbosity level
         if (!strcmp(argv[1], "-q") || !strcmp(argv[1], "--quiet")) {
-            argoffset = 1;
             verbose = 0;
+            argofs = 1;
         } else if (!strcmp(argv[1], "-v") || !strcmp(argv[1], "--verbose")) {
-            argoffset = 1;
             verbose = 2;
+            argofs = 1;
+        }
+        // Number under investigation
+        if (argc - argofs > 1) {
+            const long long a = atoll(argv[argofs + 1]);
+            if (a > 0 && a <= UINT32_MAX)
+                target = (uint32_t)a;
         }
     }
 
-    // Must have 2 or 3 command line arguments:
-    //   (optional) quiet/verbose option
-    //   target (= sum)
-    //   parts (= how many parts make up the sum)
-    if (argc - argoffset != 3) {
-        if (verbose)
-            fprintf(stderr, "Useage: %s [-q|-v] <target> <parts>\n", argv[0]);
-        return 1;
-    }
-
-    // Check argument values.
-    const long long a1 = atoll(argv[argoffset + 1]), a2 = atoll(argv[argoffset + 2]);
-    if (a1 <= 0 || a1 > TARGET_MAX) {
-        if (verbose)
-            fprintf(stderr, "Argument 1 out of range: 0 < target <= %d.\n", TARGET_MAX);
-        return 2;
-    }
-    if (a2 <= 0 || a2 > PARTS_MAX) {
-        if (verbose)
-            fprintf(stderr, "Argument 2 out of range: 0 < parts <= %d.\n", PARTS_MAX);
-        return 3;
-    }
-    if (a1 <= a2) {
-        if (verbose)
-            fprintf(stderr, "Target (arg. 1) must be greater than parts (arg. 2).\n");
-        return 4;
-    }
-
-    // Search parameters.
-    const int target = (int)a1, parts = (int)a2, N = (int)(ceil(cbrt(target * 6)));
-
-    // Final consistency check.
-    if (N <= parts) {
-        if (verbose)
-            fprintf(stderr, "Cuberoot(target x 6) must be greater than parts. Increase target or decrease parts.\n");
-        return 5;
-    }
+    // Tetrahedral root with Cardano's formula for 3rd-degree polynomials.
+    const long double r1 = (long double)target * 3, r2 = sqrtl(3) / 9, r = sqrtl((r1 + r2)) * sqrtl((r1 - r2)), ca = cbrtl(r1 + r) + cbrtl(r1 - r);
+    N = 1 + (uint32_t)(floorl(ca));  // might be 1 too high
 
     // Precompute the first N tetrahedral numbers to choose from.
-    int *tetra = malloc((size_t)N * sizeof *tetra);
-    for (int i = 0; i < N; ++i)
-        tetra[i] = tetracalc(i);
+    tetra = malloc(N * sizeof *tetra);
+    tetra[0] = 0;  // Te(0)=0
+    tetra[1] = 1;  // Te(1)=1
+    for (uint32_t n = 2; n < N; ++n)
+        tetra[n] = tetra[n - 1] - tetra[n - 2] + tetra[n - 1] + n;  // recursive relation overflows later
+    while (N > 1 && (tetra[N - 1] > target || tetra[N - 1] < tetra[N - 2]))
+        --N;  // adjust for too many, or overflow
 
-    // Check all combinations.
-    int *index = NULL, solutions = 0;
-    while ((index = combinations(N, parts))) {
-        // Skip first tetrahedral number T(0)=0 because a zero term is no term.
-        if (!index[0])
-            continue;
-        // Sum of current combination.
-        int sum = tetra[index[0]];
-        for (int i = 1; i < parts; ++i)
-            sum += tetra[index[i]];
-        // Is this a solution?
-        if (sum == target) {
-            ++solutions;
-            if (verbose == 1) {
-                printf("%d", tetra[index[parts - 1]]);
-                for (int i = parts - 1; i > 0; )
-                    printf(" %d", tetra[index[--i]]);
-                printf("\n");
-            } else if (verbose == 2) {
-                printf("%u: T(%d)", solutions, index[parts - 1]);
-                for (int i = parts - 1; i > 0; )
-                    printf("+T(%d)", index[--i]);
-                printf(" = %d", tetra[index[parts - 1]]);
-                for (int i = parts - 1; i > 0; )
-                    printf("+%d", tetra[index[--i]]);
-                printf(" = %d\n", target);
-            }
-        }
+    // Launch parallel threads.
+    threads = 0;
+#if __APPLE__
+    for (int i = 0, res = 0; !res && i < cores; ++i, ++threads) {
+        arg[i] = (uint32_t)(i + 1);
+        res = pthread_create(&tid[i], NULL, loop, &arg[i]);
+    }
+    for (uint32_t i = 0; i < threads; ++i)
+        pthread_join(tid[i], NULL);
+#elif __linux__
+    // TODO: use threads on Linux
+#endif
+
+    // Single-threaded backup if no threads.
+    if (!threads) {
+        threads = 1;
+        arg[0] = 1;
+        loop(&arg[0]);
     }
 
-    // Summary.
-    if (!verbose) {
-        if (solutions)
-            printf("%d\n", solutions);
-    } else if (verbose == 1) {
-        printf("Solutions: %d\n", solutions);
-    } else if (verbose == 2) {
-        if (solutions)
-            printf("------------------------------------------------------------------------------------------\n");
-        printf("Found %d different combinations of %d distinct tetrahedral numbers that sum to %d.\n", solutions, parts, target);
+    // Summary
+    if (verbose) {
+        if ((verbose == 2 && total) || part[0] + part[1] + part[2] + part[3])
+            printf("\n");
+        printf("Target          : %"PRIu32"\n", target);
+        printf("Sums found      : %"PRIu32"\n", total);
+        printf("All distinct    : %"PRIu32"\n", total - dup);
+        printf("With duplicates : %"PRIu32"\n", dup);
+        printf("Terms 1,2,3,4,5 : ");
     }
+    printf("%"PRIu32",%"PRIu32",%"PRIu32",%"PRIu32",%"PRIu32"\n", part[0], part[1], part[2], part[3], part[4]);
 
     free(tetra);
     return 0;
---
+++ tetrasum1.c
@@ -0,0 +1,152 @@
+/******************************************************************************
+ * Brady Haran of Numberphile asked: which 5 tetrahedral numbers sum to 343867?
+ * This is my solution that finds 277 different combinations of 5 distinct
+ * tetrahedral numbers. I implemented Donald Knuth's algorithm to find all
+ * combinations of k elements from a set of n, from The Art of Computer
+ * Programming - Volume 4A - Combinatorial Algorithms, §7.2.1.3, algorithm T.
+ *
+ * Compile: clang -std=gnu17 -Ofast -o tetrasum tetrasum.c combinations.c
+ * Run: ./tetrasum 343867 5     : show tetrahedral numbers and no. of solutions
+ *  or: ./tetrasum -v 343867 5  : show everything (verbose)
+ *  or: ./tetrasum -q 343867 5  : only show no. of solutions (quiet)
+ *
+ * E. Dronkert
+ * Utrecht, Netherlands
+ * 2024-05-20
+ * https://github.com/ednl/c/blob/master/tetrasum.c
+ *
+ * Numberphile video is out now: https://www.youtube.com/watch?v=CK3_zarXkw0
+ * Turns out this was in reference to one of Pollock's conjectures, see
+ * https://en.wikipedia.org/wiki/Pollock%27s_conjectures and 343867 is thought
+ * to be the last (largest) sum that needs five tetrahedral numbers. But they
+ * don't have to be distinct! E.g. 17 is the first (smallest) that needs more
+ * than four: 17 = 10 + 4 + 1 + 1 + 1.
+ *
+ * So unfortunately this program does not answer Brady's question because it
+ * misses solutions with duplicate tetrahedral numbers.
+ ******************************************************************************/
+
+#include <stdio.h>
+#include <stdlib.h>  // atoll, malloc, free
+#include <limits.h>  // INT_MAX
+#include <math.h>    // cbrt, ceil
+#include <string.h>  // strcmp
+
+// My own "n choose k" combinations function.
+#include "combinations.h"
+
+// Avoid overflow when determining tetrahedral numbers to choose from.
+#define TARGET_MAX (INT_MAX / 6)
+
+// Arbitrary limit, but n!/(k!(n-k)!) grows fast.
+#define PARTS_MAX 128
+
+// Calculate the n'th tetrahedral number.
+static int tetracalc(const int n)
+{
+    return ((n * (n + 1)) >> 1) * (n + 2) / 3;  // avoid early overflow by dividing in 2 steps
+}
+
+int main(int argc, char *argv[])
+{
+    // Verbosity level.
+    int argoffset = 0;
+    int verbose = 1;
+    if (argc > 1) {
+        if (!strcmp(argv[1], "-q") || !strcmp(argv[1], "--quiet")) {
+            argoffset = 1;
+            verbose = 0;
+        } else if (!strcmp(argv[1], "-v") || !strcmp(argv[1], "--verbose")) {
+            argoffset = 1;
+            verbose = 2;
+        }
+    }
+
+    // Must have 2 or 3 command line arguments:
+    //   (optional) quiet/verbose option
+    //   target (= sum)
+    //   parts (= how many parts make up the sum)
+    if (argc - argoffset != 3) {
+        if (verbose)
+            fprintf(stderr, "Usage: %s [-q|-v] <target> <parts>\n", argv[0]);
+        return 1;
+    }
+
+    // Check argument values.
+    const long long a1 = atoll(argv[argoffset + 1]), a2 = atoll(argv[argoffset + 2]);
+    if (a1 <= 0 || a1 > TARGET_MAX) {
+        if (verbose)
+            fprintf(stderr, "Argument 1 out of range: 0 < target <= %d.\n", TARGET_MAX);
+        return 2;
+    }
+    if (a2 <= 0 || a2 > PARTS_MAX) {
+        if (verbose)
+            fprintf(stderr, "Argument 2 out of range: 0 < parts <= %d.\n", PARTS_MAX);
+        return 3;
+    }
+    if (a1 <= a2) {
+        if (verbose)
+            fprintf(stderr, "Target (arg. 1) must be greater than parts (arg. 2).\n");
+        return 4;
+    }
+
+    // Search parameters.
+    const int target = (int)a1, parts = (int)a2, N = (int)(ceil(cbrt(target * 6)));
+
+    // Final consistency check.
+    if (N <= parts) {
+        if (verbose)
+            fprintf(stderr, "Cuberoot(target x 6) must be greater than parts. Increase target or decrease parts.\n");
+        return 5;
+    }
+
+    // Precompute the first N tetrahedral numbers to choose from.
+    int *tetra = malloc((size_t)N * sizeof *tetra);
+    for (int i = 0; i < N; ++i)
+        tetra[i] = tetracalc(i);
+
+    // Check all combinations.
+    int *index = NULL, solutions = 0;
+    while ((index = combinations(N, parts))) {
+        // Skip first tetrahedral number T(0)=0 because a zero term is no term.
+        if (!index[0])
+            continue;
+        // Sum of current combination.
+        int sum = tetra[index[0]];
+        for (int i = 1; i < parts; ++i)
+            sum += tetra[index[i]];
+        // Is this a solution?
+        if (sum == target) {
+            ++solutions;
+            if (verbose == 1) {
+                printf("%d", tetra[index[parts - 1]]);
+                for (int i = parts - 1; i > 0; )
+                    printf(" %d", tetra[index[--i]]);
+                printf("\n");
+            } else if (verbose == 2) {
+                printf("%u: T(%d)", solutions, index[parts - 1]);
+                for (int i = parts - 1; i > 0; )
+                    printf("+T(%d)", index[--i]);
+                printf(" = %d", tetra[index[parts - 1]]);
+                for (int i = parts - 1; i > 0; )
+                    printf("+%d", tetra[index[--i]]);
+                printf(" = %d\n", target);
+            }
+        }
+    }
+
+    // Summary.
+    if (!verbose) {
+        if (solutions)
+            printf("%d\n", solutions);
+    } else if (verbose == 1) {
+        printf("Solutions: %d\n", solutions);
+    } else if (verbose == 2) {
+        if (solutions)
+            printf("------------------------------------------------------------------------------------------\n");
+        printf("Found %d different combinations of %d distinct tetrahedral numbers that sum to %d.\n", solutions, parts, target);
+    }
+
+    free(tetra);
+    return 0;
+}
