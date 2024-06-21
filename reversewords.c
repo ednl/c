87d110ed2e0cf498030d22380a7212fccd8742d8
E. Dronkert <ednl@users.noreply.github.com>
2024-06-22 01:45:09 +0200

    reverse word order in sentence

---
+++ reversewords.c
@@ -0,0 +1,63 @@
+#include <stdio.h>
+#include <string.h>  // strlen
+#include <ctype.h>   // isspace
+#include <stdbool.h>
+
+// Reverse string from s to end (exclusive)
+static void revstr(char *s, char *end)
+{
+    while (s < --end) {
+        const char c = *s;
+        *s++ = *end;
+        *end = c;
+    }
+}
+
+// Does string contain any spaces from s to end (exclusive)
+static bool hasspace(const char *s, const char *const end)
+{
+    while (s < end)
+        if (isspace(*s++))
+            return true;
+    return false;
+}
+
+// Reverse words in string, keep char order in words
+static void reverse_words(char *s)
+{
+    // NULL or empty?
+    if (!s || !*s)
+        return;
+    char *end = s + strlen(s);  // *end == '\0'
+    // Skip spaces at begin ("ltrim")
+    while (s < end && isspace(*s))
+        ++s;
+    // Skip spaces at end ("rtrim") incl. newline
+    while (s < end && isspace(*(end - 1)))
+        --end;
+    // Anything to reverse?
+    if (!hasspace(s + 1, end - 1))
+        return;
+    // Reverse trimmed part of line char by char
+    revstr(s, end);
+    // Unreverse words
+    while (s < end) {
+        char *sep = s + 1;
+        while (sep < end && !isspace(*sep))
+            ++sep;
+        revstr(s, sep);
+        s = sep + 1;
+        while (s < end && isspace(*s))
+            ++s;
+    }
+}
+
+int main(void)
+{
+    char buf[BUFSIZ];
+    while (fgets(buf, sizeof buf, stdin) && *buf && *buf != '\n') {
+        reverse_words(buf);
+        printf("%s\n", buf);
+    }
+    return 0;
+}
