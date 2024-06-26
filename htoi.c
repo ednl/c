a2e34d0a5edd87ee07dfe14f8a75236636cb9e25
E. Dronkert <ednl@users.noreply.github.com>
2024-06-26 13:16:17 +0200

    it works but it's dead slow

--- htoi.c
+++ htoi.c
@@ -1,36 +1,73 @@
 #include <stdio.h>
+#include <ctype.h>   // isspace
 #include <stdlib.h>  // strtoul
 #include <limits.h>  // INT_MAX
+#include <stdbool.h>
+#include "startstoptimer.h"
 
-#define MAXHEXDIGITS (sizeof(unsigned) << 1)
+// Two hex digits per byte
+#define HEXDIGITS (sizeof(unsigned) << 1)
+#define TOPNIBBLE (0xFU << ((HEXDIGITS - 1) << 2))
 
+// c >= 32 && c < 128
+static inline bool readable_ascii(const char c)
+{
+    return c & 0x60;
+}
+
+// Overflow if left-shifted by 4
+static inline bool overflow_danger(const unsigned x)
+{
+    return x & TOPNIBBLE;
+}
+
+// c in [0..9,A..F,a..f]
+static inline bool ishexdigit(const char c)
+{
+    return readable_ascii(c) && ((c >= '0' && c <= '9') || (c >= 'A' && c <= 'F') || (c >= 'a' && c <= 'f'));
+}
+
+// Hexadecimal ASCII byte string to (unsigned) int
 static unsigned htoi(const char *s)
 {
     if (!s)
         return 0;
-    if (*s == '0' && (*(s + 1) == 'x' || *(s + 1) == 'X'))
-        s += 2;
+    while (isspace(*s))  // skip leading spaces
+        ++s;
+    if (*s == '0' && (*(s + 1) | 0x20) == 'x')  // set bit 5 = to lower case
+        s += 2;  // skip "0x" hex prefix
     unsigned x = 0;
-    for (size_t i = 0; i < MAXHEXDIGITS; ++i, ++s)
-        if (*s >= '0' && *s <= '9')
-            x = x << 4 | (*s & 0xf);
-        else if ((*s >= 'A' && *s <= 'F') || (*s >= 'a' && *s <= 'f'))
-            x = x << 4 | ((*s & 0x7) + 9);
+    while (readable_ascii(*s) && !overflow_danger(x)) {
+        const char c = *s++ | 0x20;  // doesn't affect '0'..'9' and 0x10..0x19 already excluded
+        if (c >= '0' && c <= '9')
+            x = x << 4 | (c & 0xF);  // times 16, add 0..9
+        else if (c >= 'a' && c <= 'f')
+            x = x << 4 | ((c & 0x7) + 9);  // times 16, add 10..15
         else
-            return x;
-    return x;
+            return x;  // stop at first non-hexdigit
+    }
+    return ishexdigit(*s) ? UINT_MAX : x;  // too many hex digits = error
 }
 
-static unsigned htoi2(const char *s)
+static unsigned htoi_alt(const char *s)
 {
     unsigned long x = strtoul(s, NULL, 16);
-    while (x > UINT_MAX)
-        x >>= 4;
-    return x;
+    return x > UINT_MAX ? UINT_MAX : x;
 }
 
 int main(void)
 {
-    printf("0x%X\n", htoi("CafeBabe"));
+    volatile unsigned x;
+
+    starttimer();
+    for (int i = 0; i < 1000000000; ++i)
+        x = htoi(" 0xCafeBabe");
+    printf("%.2f s\n", stoptimer_s());
+
+    starttimer();
+    for (int i = 0; i < 1000000000; ++i)
+        x = htoi_alt(" 0xCafeBabe");
+    printf("%.2f s\n", stoptimer_s());
+
     return 0;
 }
