7dc6313522be7eae8b1a1c097cb51e32bb71677b
E. Dronkert <ednl@users.noreply.github.com>
2024-07-21 05:04:30 +0200

    layout

--- fizzbuzz-fast.c
+++ fizzbuzz-fast.c
@@ -1,8 +1,7 @@
 #include <stdio.h>
-int main(void)
-{
+int main(void) {
     printf("1 2 fizz 4 buzz fizz 7 8 fizz buzz\n");
-    for (unsigned long long i = 1;; i += 3) {
+    for (unsigned long long i = 1;; i += 3)
         printf("%1$llu1 fizz %1$llu3 %1$llu4 fizzbuzz "
             "%1$llu6 %1$llu7 fizz %1$llu9 buzz\n"
             "fizz %2$llu2 %2$llu3 fizz buzz "
@@ -10,5 +9,4 @@ int main(void)
             "%3$llu1 %3$llu2 fizz %3$llu4 buzz "
             "fizz %3$llu7 %3$llu8 fizz buzz\n",
             i, i + 1, i + 2);
-    }
 }
