f170ecad12892fd26eaa1cebb5d2a2bf23827589
E. Dronkert <ednl@users.noreply.github.com>
2024-09-02 05:18:15 +0200

    shorter

--- dups-nosort.c
+++ dups-nosort.c
@@ -1,25 +1,14 @@
-#include <stdio.h>   // printf
-#include <stdlib.h>  // rand
-
-#define N 10  // list length
-#define M  5  // element range
-
-static int a[N];
-
+#include <stdio.h>
+static char a[BUFSIZ];
 int main(void)
 {
-    printf("Input:");
-    for (int i = 0; i < N; ++i)
-        printf(" %d", a[i] = 1 + rand() % M);
-    printf("\n");
-
+    fgets(a, sizeof a, stdin);
     int dup = 0;
-    for (int i = 0; i < N - 1; ++i)
-        for (int j = 0, seen = 0; j < N; ++j) {
+    for (int i = 0; a[i]; ++i)
+        for (int j = 0, seen = 0; a[j]; ++j) {
             if (i > j && a[i] == a[j])
                 seen = 1;
             dup += !seen && i < j && a[i] == a[j];
         }
     printf("%d\n", dup);
-    return 0;
 }
