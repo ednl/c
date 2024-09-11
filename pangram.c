ea0adb78905c58215857b685d6d4116387d7a6a3
E. Dronkert <ednl@users.noreply.github.com>
2024-09-11 04:37:53 +0200

    pangram

--- pangram.c
+++ pangram.c
@@ -7,16 +7,30 @@
 
 int main(int argc, char *argv[])
 {
-    int c, found = 0;                    // ints must be at least 32-bit for this to work
-    while ((c = fgetc(stdin)) != EOF) {  // get char from standard input
-        c |= 1 << 5;                     // set bit 5 = tolower for A-Z (assumes input is ASCII)
-        if (c >= 'a' && c <= 'z') {      // is the current char a letter?
-            found |= 1 << (c - 'a');     // set bit corresponding to this letter
-            if (found == (1 << 26) - 1)  // are all 26 bits set?
-                return 0;                // 0 = success
+    int found = 0;
+    if (isatty(fileno(stdin))) {
+        // No pipe or redirect, try command line arguments
+        for (int i = 1; i < argc; ++i) {
+            const char *s = argv[i];
+            while (*s) {
+                const char c = *s++ | (1 << 5);
+                if (c >= 'a' && c <= 'z' && (found |= 1 << (c - 'a')) == (1 << 26) - 1)
+                    return 0;
+            }
+        }
+    } else {
+        // Input is pipe or redirect to stdin of this program
+        int c;
+        while ((c = fgetc(stdin)) != EOF) {  // get char from standard input
+            c |= 1 << 5;                     // set bit 5 = tolower for A-Z (assumes input is ASCII)
+            if (c >= 'a' && c <= 'z') {      // is the current char a letter?
+                found |= 1 << (c - 'a');     // set bit corresponding to this letter
+                if (found == (1 << 26) - 1)  // are all 26 bits set?
+                    return 0;                // 0 = success
+            }
         }
     }
-    for (c = 'a'; c <= 'z'; ++c, found >>= 1)
+    for (int c = 'a'; c <= 'z'; ++c, found >>= 1)
         if (!(found & 1))
             fputc(c, stderr);
     fputc('\n', stderr);
