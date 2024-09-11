1a0944302dea2f283ffe2b557ed3ae871c8a6bec
E. Dronkert <ednl@users.noreply.github.com>
2024-09-11 22:23:41 +0200

    pamgram

--- pangram.c
+++ pangram.c
@@ -1,38 +1,50 @@
 /**
- * Determines if input is a 'pangram'
+ * Determines if input contains all letters of the (ASCII) alphabet
+ * as described by https://en.wikipedia.org/wiki/Pangram
  */
 
 #include <stdio.h>
 #include <unistd.h>   // isatty, fileno
 
-int main(int argc, char *argv[])
+#define PANGRAM ((1 << 26) - 1)
+#define TOLOWER (1 << 5)
+
+static int letters(const char *s)
 {
     int found = 0;
-    if (isatty(fileno(stdin))) {
-        // No pipe or redirect, try command line arguments
-        for (int i = 1; i < argc; ++i) {
-            const char *s = argv[i];
-            while (*s) {
-                const char c = *s++ | (1 << 5);
-                if (c >= 'a' && c <= 'z' && (found |= 1 << (c - 'a')) == (1 << 26) - 1)
-                    return 0;
-            }
-        }
-    } else {
-        // Input is pipe or redirect to stdin of this program
-        int c;
-        while ((c = fgetc(stdin)) != EOF) {  // get char from standard input
-            c |= 1 << 5;                     // set bit 5 = tolower for A-Z (assumes input is ASCII)
-            if (c >= 'a' && c <= 'z') {      // is the current char a letter?
-                found |= 1 << (c - 'a');     // set bit corresponding to this letter
-                if (found == (1 << 26) - 1)  // are all 26 bits set?
-                    return 0;                // 0 = success
-            }
-        }
+    while (*s) {
+        const char c = *s++ | TOLOWER;
+        if (c >= 'a' && c <= 'z' && (found |= 1 << (c - 'a')) == PANGRAM)
+            return PANGRAM;
     }
+    return found;
+}
+
+int main(int argc, char *argv[])
+{
+    char buf[BUFSIZ];
+    int found = 0;
+    if (!isatty(fileno(stdin))) {
+        // Input is pipe or redirect to stdin of this program
+        while (fgets(buf, sizeof buf, stdin))
+            if ((found |= letters(buf)) == PANGRAM)
+                return 0;
+    } else if (argc > 1) {
+        // Command line arguments
+        for (int i = 1; i < argc; ++i)
+            if ((found |= letters(argv[i])) == PANGRAM)
+                return 0;
+    } else {
+        // Manual input
+        printf("? ");
+        if (fgets(buf, sizeof buf, stdin) && (found = letters(buf)) == PANGRAM)
+            return 0;
+    }
+
+    // Report missing letters
     for (int c = 'a'; c <= 'z'; ++c, found >>= 1)
         if (!(found & 1))
             fputc(c, stderr);
     fputc('\n', stderr);
-    return 1;                            // 1 = error
+    return 1;  // not a pangram
 }
