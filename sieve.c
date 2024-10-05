150cd6842a02553baa2f0c9445c1a038dde7dc02
E. Dronkert <ednl@users.noreply.github.com>
2024-10-05 02:43:57 +0200

    sieve

--- sieve.c
+++ sieve.c
@@ -1,23 +1,38 @@
-#include <stdio.h>
-#include <stdint.h>
-#include <stdbool.h>
+#include <stdio.h>    // printf
+#include <stdlib.h>   // malloc
+#include <stdbool.h>  // bool, true
 
-#define N 100
-uint64_t p[N] = {2, 3};
-
-int main(void)
+int main(int argc, char *argv[])
 {
-    int k = 0;
-    for (int i = 2; i < N; ++i) {
-        bool notfound = true;
-        for (uint64_t q = p[i - 1] + 2; notfound; q += 2) {
-            while (k < i && p[k] * p[k] <= q)
-                ++k;
-            bool isprime = true;
-            for (int j = 1; isprime && j < k; ++j)
-                isprime = isprime && q % p[j];
-            p[i] = q;
+    if (argc != 2)
+        return 1;
+    int n = atoi(argv[1]);
+    if (n < 1)
+        return 2;
+    unsigned *p = malloc(n * sizeof *p);
+    if (!p)
+        return 3;
+
+    p[0] = 2;
+    p[1] = 3;
+    if (n < 3)
+        goto done;
+
+    int i = 2, k = 0;
+    for (unsigned q = 5, r = 4;; q += 2) {
+        for (; r <= q; ++k, r = p[k] * p[k]);
+        bool isprime = true;
+        for (int j = 1; isprime && j < k; ++j)
+            isprime = isprime && q % p[j];
+        if (isprime) {
+            p[i++] = q;
+            if (i == n)
+                break;
         }
     }
+
+done:;
+    printf("P%d = %u\n", n, p[n - 1]);
+    free(p);
     return 0;
 }
