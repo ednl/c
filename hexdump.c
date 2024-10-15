9377c3142801cc9d22d5af59e013693b5216ae80
E. Dronkert <ednl@users.noreply.github.com>
2024-10-15 13:44:19 +0200

    formatting

--- hexdump.c
+++ hexdump.c
@@ -1,26 +1,43 @@
-#include <stdio.h>
+#include <stdio.h>   // printf
+#include <string.h>  // strlen
 
-static char *dump(char *const buf, const unsigned char *bytes, size_t count)
+// buf size must be greater than count*3
+static char *dump(char *const buf, const unsigned char *bytes, int count)
 {
     static const char *hex = "0123456789abcdef";
-    if (!buf || !bytes || !count)
+    if (!buf || !bytes || count < 1)
         return NULL;
     char *s = buf;
-    while (count--) {
-        *s++ = hex[*bytes >> 4 & 0x0f];
-        *s++ = hex[*bytes++    & 0x0f];
+    while (count-- > 0) {
         *s++ = ' ';
+        *s++ = hex[*bytes >> 4];
+        *s++ = hex[*bytes++ & 0x0f];
     }
-    *(s - 1) = '\n';
     *s = '\0';
     return buf;
 }
 
+static int min(const int a, const int b)
+{
+    return a < b ? a : b;
+}
+
+static int max(const int a, const int b)
+{
+    return a > b ? a : b;
+}
+
 int main(int argc, char *argv[])
 {
-    const size_t count = 8;
-    char buf[BUFSIZ];  // size must be greater than count*3
-    if (argc > 1)
-        printf("%s", dump(buf, (unsigned char *)argv[1], count));
+    char buf[BUFSIZ];
+    const int maxcount = (BUFSIZ - 1) / 3;
+
+    int maxwidth = 0;
+    for (int i = 1; i < argc; ++i)
+        maxwidth = max(maxwidth, min(strlen(argv[i]), maxcount));
+
+    for (int i = 1; i < argc; ++i)
+        printf("%2d |%s | %-*s |\n", i, dump(buf, (unsigned char *)argv[i], maxwidth + 1), maxwidth, argv[i]);
+
     return 0;
 }
--- lim.c
+++ lim.c
@@ -1,41 +1,43 @@
-#include <stdio.h>
-#include <stdint.h>
-#include <inttypes.h>
-#include <limits.h>
-#include <float.h>
-// #include <math.h>
+#include <stdio.h>     // printf
+#include <stddef.h>    // size_t, ptrdiff_t
+#include <stdint.h>    // int_fast*
+#include <limits.h>    // CHAR_BIT
+#include <float.h>     // FLT_*, DBL_*, LDBL_*
 
 int main(void)
 {
-    printf("\n----- Char is Signed or Unsigned ------\n");
+    printf("\n----- Char is Signed or Unsigned --------\n");
     printf("char == %ssigned char\n", CHAR_MAX == SCHAR_MAX ? "" : "un");
 
-    printf("\n----- Integer Sizes -------------------\n");
-    printf("char         = %zu\n", sizeof(char     ) * CHAR_BIT);
-    printf("short        = %zu\n", sizeof(short    ) * CHAR_BIT);
-    printf("int          = %zu\n", sizeof(int      ) * CHAR_BIT);
-    printf("long         = %zu\n", sizeof(long     ) * CHAR_BIT);
-    printf("long long    = %zu\n", sizeof(long long) * CHAR_BIT);
-    printf("size_t       = %zu\n", sizeof(size_t   ) * CHAR_BIT);
-    printf("intptr_t     = %zu\n", sizeof(intptr_t ) * CHAR_BIT);
-    printf("intmax_t     = %zu\n", sizeof(intmax_t ) * CHAR_BIT);
+    printf("\n----- Integer Sizes ---------------------\n");
+    printf("char             = %3zu\n", sizeof(char     ) * CHAR_BIT);
+    printf("short            = %3zu\n", sizeof(short    ) * CHAR_BIT);
+    printf("int              = %3zu\n", sizeof(int      ) * CHAR_BIT);
+    printf("long             = %3zu\n", sizeof(long     ) * CHAR_BIT);
+    printf("long long        = %3zu\n", sizeof(long long) * CHAR_BIT);
+    printf("size_t           = %3zu\n", sizeof(size_t   ) * CHAR_BIT);
+    printf("ptrdiff_t        = %3zu\n", sizeof(ptrdiff_t) * CHAR_BIT);
+    printf("intptr_t         = %3zu\n", sizeof(intptr_t ) * CHAR_BIT);
+    printf("intmax_t         = %3zu\n", sizeof(intmax_t ) * CHAR_BIT);
 
-    printf("\n----- Fast Integer Sizes --------------\n");
-    printf("int_fast8_t  = %zu\n", sizeof(int_fast8_t ) * CHAR_BIT);
-    printf("int_fast16_t = %zu\n", sizeof(int_fast16_t) * CHAR_BIT);
-    printf("int_fast32_t = %zu\n", sizeof(int_fast32_t) * CHAR_BIT);
-    printf("int_fast64_t = %zu\n", sizeof(int_fast64_t) * CHAR_BIT);
+    printf("\n----- Fast Integer Sizes ----------------\n");
+    printf("int_fast8_t      = %3zu\n", sizeof(int_fast8_t ) * CHAR_BIT);
+    printf("int_fast16_t     = %3zu\n", sizeof(int_fast16_t) * CHAR_BIT);
+    printf("int_fast32_t     = %3zu\n", sizeof(int_fast32_t) * CHAR_BIT);
+    printf("int_fast64_t     = %3zu\n", sizeof(int_fast64_t) * CHAR_BIT);
 
-    printf("\n----- Floating Point Types ------------\n");
-    printf("float            = %zu\n", sizeof(float      ) * CHAR_BIT);
-    printf("double           = %zu\n", sizeof(double     ) * CHAR_BIT);
-    printf("long double      = %zu\n", sizeof(long double) * CHAR_BIT);
+    printf("\n----- Floating Point Sizes --------------\n");
+    printf("float            = %3zu\n", sizeof(float      ) * CHAR_BIT);
+    printf("double           = %3zu\n", sizeof(double     ) * CHAR_BIT);
+    printf("long double      = %3zu\n", sizeof(long double) * CHAR_BIT);
+
+    printf("\n----- Floating Point Implementation -----\n");
     printf("FLT_RADIX        = %d\n", FLT_RADIX);
     printf("FLT_ROUNDS       = %d (-1,0,1,2,3: ?,to 0,nearest,+inf,-inf)\n", FLT_ROUNDS);
     printf("FLT_EVAL_METHOD  = %d (-1,0,1,2: ?,none,double,long double)\n", FLT_EVAL_METHOD);
     printf("FLT_HAS_SUBNORM  = %d (-1,0,1: ?,no,yes)\n", FLT_HAS_SUBNORM);
 
-    printf("\n----- Float Limits --------------------\n");
+    printf("\n----- Float Limits ----------------------\n");
     printf("FLT_DECIMAL_DIG  = %d\n", FLT_DECIMAL_DIG);
 #pragma clang diagnostic push
 #pragma clang diagnostic ignored "-Wdouble-promotion"
@@ -51,7 +53,7 @@ int main(void)
     printf("FLT_MAX_EXP      = %d\n", FLT_MAX_EXP);
     printf("FLT_MAX_10_EXP   = %d\n", FLT_MAX_10_EXP);
 
-    printf("\n----- Double Limits -------------------\n");
+    printf("\n----- Double Limits ---------------------\n");
     printf("DBL_DECIMAL_DIG  = %d\n", DBL_DECIMAL_DIG);
     printf("DBL_MIN          = %.8e\n", DBL_MIN);
     printf("DBL_TRUE_MIN     = %.8e\n", DBL_TRUE_MIN);
@@ -64,7 +66,7 @@ int main(void)
     printf("DBL_MAX_EXP      = %d\n", DBL_MAX_EXP);
     printf("DBL_MAX_10_EXP   = %d\n", DBL_MAX_10_EXP);
 
-    printf("\n----- Long Double Limits --------------\n");
+    printf("\n----- Long Double Limits ----------------\n");
     printf("LDBL_DECIMAL_DIG = %d\n", LDBL_DECIMAL_DIG);
     printf("LDBL_MIN         = %.8Le\n", LDBL_MIN);
     printf("LDBL_TRUE_MIN    = %.8Le\n", LDBL_TRUE_MIN);
