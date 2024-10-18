e02298c3f3571ba84c56df14c86ff7b2b4907789
E. Dronkert <ednl@users.noreply.github.com>
2024-10-18 21:01:49 +0200

    no more errors in math

--- taylor.c
+++ taylor.c
@@ -9,57 +9,65 @@
  * calculate the series sum for a given x and epsilon.
  *
  * Observe that each term T(n) of the partial sum S is:
- *   T(n) = T(n-2) * -1 * x^2 * (n-2) / ((n-1) * n)
- * where n=3,5,7,... and T(1)=x.
- *
- * Integer overflow in the coefficients should not be an issue because
- * the series converges fast enough for reasonably small x, but still,
- * to avoid the n*(n-1) multiplication:
+ *   T(0) = x
+ *   T(n) = T(n-1) * -1 * x^2 * (2n-1)^2 / (2n * (2n+1))
+ * for n=1,2,3,...
  *
  * Partial fraction decomposition of the extra coefficient factor per term:
- *   (n-2)/((n-1)n) = A/(n-1) + B/n
- *   n-2 = An + B(n-1)
- *   n=1 => A = -1
- *   n=0 => B = 2
- *   => (n-2)/((n-1)n) = -1/(n-1) + 2/n
+ *   (2n-1)^2 / 2n(2n+1)
+ *   = ((2n+1)^2 - 8n) / 2n(2n+1)
+ *   = (2n+1)/2n - 8n / 2n(2n+1)
+ *   = 1 + 1/2n - 4/(2n+1)
  */
 
 #include <stdio.h>  // printf
 #include <math.h>   // log, sqrt
+#include <float.h>  // DBL_DECIMAL_DIG
+
+static double epsilon(const int dec)
+{
+    return pow(10, -dec);
+}
 
 static double f(const double x)
 {
     return log(x + sqrt(1 + x * x));
 }
 
-static void progress(const int ord, const double coef, const double term, const double sum)
+static void progress(const int index, const double term, const double sum, const int dec)
 {
-    printf("ord %2d coef %.12f term %+.12f sum %.12f\n", ord, coef, term, sum);
+    printf("%3d: %+.*f = %.*f\n", index, dec + 1, term, dec, sum);
+}
+
+static double estimate(const double x, const int dec)
+{
+    const double eps = epsilon(dec);
+    double term = x;
+    double sum = x;
+    if (dec > 0 && dec <= DBL_DECIMAL_DIG)
+        progress(0, term, sum, dec);
+
+    const double mx2 = -x * x;
+    for (int n = 1; fabs(term) >= eps && n < 1000; ++n) {
+        const int n2 = n << 1;
+        term *= mx2 * (1.0 / n2 - 4.0 / (n2 + 1) + 1);
+        sum += term;
+        if (dec > 0 && dec <= DBL_DECIMAL_DIG)
+            progress(n, term, sum, dec);
+    }
+    return sum;
 }
 
 int main(void)
 {
-    // Input
-    const double x = 20.0;
-    const double eps = 1e-8;
-    printf("x = %.3f eps = %.0e\nf(x) = %.12f\n\n", x, eps, f(x));
+    const int dec = 6;
+    const double x = 0.5;
+    const double y = f(x);
 
-    // First term
-    int ord = 1;
-    double coef = 1.0;
-    double term = x;
-    double sum = x;
-    progress(ord, coef, term, sum);
-
-    // Next terms
-    const double mx2 = -x * x;
-    while (fabs(term) > eps) {
-        ord += 2;
-        coef *= 2.0 / ord - 1.0 / (ord - 1);  // see fraction decomposition above
-        term *= mx2 * coef;
-        sum += term;
-        progress(ord, coef, term, sum);
-    }
-
-    return 0;
+    printf("\n  x  = %.3f\n", x);
+    printf("f(x) = %.*f\n", dec + 2, y);
+    printf("   +/- %.*f\n\n", dec + 3, 0.005 * epsilon(dec));
+    const double est = estimate(x, dec);  // also print progress
+    printf("\n est = %.*f\n", dec, est);
+    printf("   +/- %.*f\n\n", dec, epsilon(dec));
 }
