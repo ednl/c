ed2b20946ba6fc90664c6f806a7178fd999c0769
E. Dronkert <ednl@users.noreply.github.com>
2024-11-28 18:08:14 +0100

    comments

--- steinmetz.c
+++ steinmetz.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>   // drand48
 #include <math.h>     // sqrt
 #include <time.h>     // time (for srand48)
+#include <stdbool.h>  // bool
 
 // Radius and diameter of all three cylinders of the Steinmetz solid
 #define R 1.0
@@ -11,51 +12,64 @@
 #define ERROR 0.005
 #define BATCH 10000
 
-// Monte Carlo volume: cube tightly enclosing tricylinder
+// Monte Carlo reference volume: cube that encloses tricylinder
 #define CUBE (D * D * D)
 
-// Volume of tricylinder with r=1: 8(2-sqrt(2))
+// Volume of tricylinder for r=1 is (2-sqrt(2)).(2r)^3
 #define CALC ((2 - M_SQRT2) * CUBE)
 
+// Random coordinate inside double unit cube around origin
 static double rnd(void)
 {
     return drand48() * 2 - 1;
 }
 
+// Convenience function for squaring
 static inline double sqr(const double x)
 {
     return x * x;
+}
+
+// Evaluate next Monte Carlo sample: inside (true) or out (false)
+static bool sample(void)
+{
+    // Probe point inside double unit cube, squared
+    const double a = sqr(rnd());
+    const double b = sqr(rnd());
+    const double c = sqr(rnd());
+    // Evaluate hit or miss
+    return a + b <= R && a + c <= R && b + c <= R;
 }
 
 int main(void)
 {
     // Init
     srand48(time(NULL));
-    unsigned hit = 0, count = 0;
-    double mean = 0, M2 = 0, stddev;
+    unsigned hit = 0, count = 0;      // Monte Carlo simulation counters
+    double mean = 0, M2 = 0, stddev;  // running mean, unscaled variance, standard deviation
 
-    // Iterate until error small enough
+    // Monte Carlo method until standard deviation is small enough
     do {
+        // Single iteration wouldn't change stddev much
         for (int i = 0; i < BATCH; ++i) {
+            // Get new Monte Carlo sample
             ++count;
-            // Probe point inside unit cube, squared
-            const double x = sqr(rnd());
-            const double y = sqr(rnd());
-            const double z = sqr(rnd());
-            // Evaluate hit or miss
-            hit += x + y <= 1 && x + z <= 1 && y + z <= 1;
+            hit += sample();
             // Currently estimated value
-            const double est = ((double)hit / count) * CUBE;
+            const double frac = (double)hit / count;
+            const double estm = frac * CUBE;
             // Update running mean and unscaled variance
-            const double delta = est - mean;
+            // https://en.wikipedia.org/wiki/Algorithms_for_calculating_variance#Welford%27s_online_algorithm
+            const double delta = estm - mean;
             mean += delta / count;
-            M2 += delta * (est - mean);
+            M2 += delta * (estm - mean);
         }
-        // Scale variance & take square root for standard deviation
+        // Scale as population variance (= exact variance of given data)
+        // and take square root for new standard deviation
         stddev = sqrt(M2 / count);
     } while (stddev > ERROR);
 
-    // Result
+    // Results
     printf("calculation =  %.5f\n", CALC);
     printf("monte carlo =  %.3f ± %.3f (N=%u)\n", mean, stddev, count);
     printf("      error = %+.4f\n", mean - CALC);
