68e44a3a1b39cadf1563292c31b27cd45e86222b
E. Dronkert <ednl@users.noreply.github.com>
2025-07-17 02:18:32 +0200

    use ctype.h

--- cs50_2_substitution.c
+++ cs50_2_substitution.c
@@ -1,53 +1,34 @@
 // https://cs50.harvard.edu/x/psets/2/substitution/
 #include <stdio.h>
 #include <stdbool.h>
+#include <ctype.h>
 
-#define KEYLEN 26
+#define KEYLEN ('Z' - 'A' + 1)
 #define BUFLEN 256
-
-static bool isupper_ascii(const char c)
-{
-    return c >= 'A' && c <= 'Z';
-}
-
-static bool islower_ascii(const char c)
-{
-    return c >= 'a' && c <= 'z';
-}
-
-static char toupper_ascii(const char c)
-{
-    return islower_ascii(c) ? c - 'a' + 'A' : c;
-}
-
-static char tolower_ascii(const char c)
-{
-    return isupper_ascii(c) ? c - 'A' + 'a' : c;
-}
 
 static bool isvalidkey(const char *key)
 {
-    bool inkey[KEYLEN] = {0};
+    bool set[KEYLEN] = {0};
     int len = 0;
-    for (const char *c = key; *c && len <= KEYLEN; ++c, ++len) {
-        int i = -1;  // invalid
-        if (isupper_ascii(*c))
+    for (const char *c = key; *c; ++c, ++len) {
+        int i = -1;  // invalid value
+        if (isupper(*c))
             i = *c - 'A';
-        else if (islower_ascii(*c))
+        else if (islower(*c))
             i = *c - 'a';
-        if (i < 0 || i >= KEYLEN || inkey[i])
+        if (i < 0 || i >= KEYLEN || set[i])
             return false;
-        inkey[i] = true;
+        set[i] = true;
     }
     return len == KEYLEN;
 }
 
 static char encrypt(const char *key, const char c)
 {
-    if (isupper_ascii(c))
-        return toupper_ascii(key[c - 'A']);
-    if (islower_ascii(c))
-        return tolower_ascii(key[c - 'a']);
+    if (isupper(c))
+        return toupper(key[c - 'A']);
+    if (islower(c))
+        return tolower(key[c - 'a']);
     return c;
 }
 
@@ -62,12 +43,11 @@ int main(int argc, char *argv[])
         fprintf(stderr, "Invalid key.\n");
         return 1;
     }
-
     char buf[BUFLEN] = {0};
     printf("plaintext:  ");
     fgets(buf, sizeof buf, stdin);
     printf("ciphertext: ");
-    for (const char *c = buf; *c && *c != '\r' && *c != '\n'; ++c)
+    for (const char *c = buf; isprint(*c); ++c)
         fputc(encrypt(key, *c), stdout);
     fputc('\n', stdout);
     return 0;
