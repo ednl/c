6c2fedf7da531adafbd9868d4dbdf877748004da
E. Dronkert <ednl@users.noreply.github.com>
2025-09-17 12:25:23 +0200

    comments

--- comments.c
+++ comments.c
@@ -1,4 +1,9 @@
-char *s = "\\\"/*";
-/* normal comment */// new style comment
+/**
+ * Multiline comment
+ */
+
+/* normal comment */
+char *s = "\\\"/*";  // new style comment
+
 /*/* tricky comment //*////**/x/**//**/y/**//**//**/z/*
 int x = 0; //*//*
