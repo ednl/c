882c1ea497b88190157bad3b63ec5bdfaf9907f0
E. Dronkert <ednl@users.noreply.github.com>
2025-10-08 12:19:50 +0200

    avoid do-while

--- sqrt-newton.c
+++ sqrt-newton.c
@@ -1,14 +1,16 @@
 /**
- * Square root:
- *     sqrt(a) = x
- *     => x^2 = a
- *     => f(x) = x^2 - a
- *             = 0
- * Newton-Raphson:
- *     x_n+1 = x_n - f(x_n) / f'(x_n)
- *           = x_n - (x_n ^2 - a) / (2.x_n)
- *           = (x_n ^2 + a)/(2.x_n)
- *           = (x_n + a/x_n)/2
+ * Find square root of a:
+ *   sqrt(a) = x
+ *   => x^2 = a
+ *   => x^2 - a = 0
+ * Solve f(x)=0 for f(x) = x^2 - a
+ *
+ * Newton-Raphson
+ * https://en.wikipedia.org/wiki/Newton%27s_method
+ * x_n+1 = x_n - f(x_n) / f'(x_n)
+ *       = x_n - (x_n ^2 - a) / (2.x_n)
+ *       = (x_n ^2 + a)/(2.x_n)
+ *       = (x_n + a/x_n)/2
  */
 
 #include <stdio.h>  // printf, fgets, sscanf
@@ -16,31 +18,28 @@
 
 static double ask_dbl_pos(const char *msg)
 {
-    char buf[BUFSIZ];
-    double x;
-    do {
-        do {
-            printf("%s: ", msg);
-            fgets(buf, sizeof buf, stdin);
-        } while (sscanf(buf, "%lf", &x) != 1);
-    } while (x <= 0);
+    char buf[BUFSIZ] = {0};
+    double x = 0;
+    for (int res = 0; res != 1 || x <= 0; ) {
+        printf("%s: ", msg);
+        if (fgets(buf, sizeof buf, stdin))
+            res = sscanf(buf, "%lf", &x);
+    }
     return x;
 }
 
 int main(void)
 {
-    const double frac = 0.00001;
-    double val = ask_dbl_pos("Positive number");
+    const double prec = 1e-5;  // arbitrary precision cut-off
+    double val  = ask_dbl_pos("Positive number");
     double root = ask_dbl_pos("First sqrt guess");
-    double dif, thr;
-    int i = 0;
-    do {
+    int step = 0;
+    for (double diff = 0, goal = 0; diff >= goal; ++step) {
         const double next = (root + val / root) / 2;
-        dif = fabs(next - root);
-        thr = fabs(frac * root);
+        diff = fabs(next - root);
+        goal = fabs(prec * root);  // fabs unnecessary for sqrt function
         root = next;
-        ++i;
-    } while (dif >= thr);
-    printf("After %d steps: sqrt(%f) = %f\n", i, val, root);
+    }
+    printf("After %d steps: sqrt(%.1f) = %.8f\n", step, val, root);
     return 0;
 }
