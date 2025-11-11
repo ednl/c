4e4ef1e4b05a1b75c6988a9708ab13dedabdbbc5
E. Dronkert <ednl@users.noreply.github.com>
2025-11-11 18:21:40 +0100

    more correct printint, and use write

--- printint.c
+++ printint.c
@@ -1,61 +1,52 @@
-#include <stdio.h>   // fwrite
+#include <unistd.h>  // write, STDOUT_FILENO
 #include <stdlib.h>  // div
 
-#define ALLOWNEGATIVE 1
-
-static void printint(int x)
+static ssize_t printint(int x, const char term)
 {
-    char buf[16];  // 32-bit signed number in base 10 takes max 11 chars
-    int i = sizeof buf;
-#if ALLOWNEGATIVE
-    char sign = 0;
-    if (x < 0) {
-        sign = '-';
-        x = -x;
-    }
-#endif
+    char buf[sizeof x * 4], *end = buf + sizeof buf, *pc = end;
+    const int sign = x < 0 ? -1 : 1;
+    if (term)
+        *--pc = term;
 	do {
         const div_t qr = div(x, 10);
-		buf[--i] = qr.rem | '0';
+		*--pc = '0' + sign * qr.rem;  // remainder of negative number is negative
 		x = qr.quot;
 	} while (x);
-#if ALLOWNEGATIVE
-    if (sign)
-        buf[--i] = sign;
-#endif
-    fwrite(buf + i, sizeof buf - i, 1, stdout);
+    if (sign == -1)
+        *--pc = '-';
+    ssize_t n, sum = 0;
+    while (end - pc > 0 && (n = write(STDOUT_FILENO, pc, end - pc)) != -1) {  // may set errno
+        sum += n;
+        pc += n;
+    }
+    return sum;
 }
 
-static char *sprintint(char *s, int x)
-{
-    char buf[16];
-    int i = 0;
-#if ALLOWNEGATIVE
-    if (x < 0) {
-        *s++ = '-';
-        x = -x;
-    }
-#endif
-	do {
-        const div_t qr = div(x, 10);
-		buf[i++] = qr.rem | '0';
-		x = qr.quot;
-	} while (x);
-    while (i)
-        *s++ = buf[--i];
-    return s;
-}
+// static char *sprintint(char *s, int x)
+// {
+//     char buf[sizeof x * 4];
+//     const int sign = x < 0 ? -1 : 1;
+//     int i = 0;
+// 	do {
+//         const div_t qr = div(x, 10);
+// 		buf[i++] = '0' + sign * qr.rem;  // remainder of negative number is negative
+// 		x = qr.quot;
+// 	} while (x);
+//     if (sign == -1)
+//         *s++ = '-';
+//     while (i)
+//         *s++ = buf[--i];
+//     return s;
+// }
 
 int main(void)
 {
-    printint(123);
-    fputc('\n', stdout);
-    printint(-321);
-    fputc('\n', stdout);
-    char buf[32] = "x=";
-    char *end = sprintint(buf + 2, -456);
-    *end = '\n';
-    *(end + 1) = '\0';
-    printf("%s", buf);
+    printint(123, '\n');
+    printint(-321, '\n');
+    // char buf[32] = "x=";
+    // char *end = sprintint(buf + 2, -456);
+    // *end = '\n';
+    // *(end + 1) = '\0';
+    // printf("%s", buf);
     return 0;
 }
