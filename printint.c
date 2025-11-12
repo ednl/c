5fcb9ec66f6332db3568714f4790dfeb60b2ed0f
E. Dronkert <ednl@users.noreply.github.com>
2025-11-12 14:39:18 +0100

    use abs instead of times sign

--- printint.c
+++ printint.c
@@ -1,52 +1,62 @@
-#include <unistd.h>  // write, STDOUT_FILENO
-#include <stdlib.h>  // div
+#include <stdio.h>    // printf
+#include <unistd.h>   // write, STDOUT_FILENO
+#include <stdlib.h>   // div, abs
+#include <stdbool.h>  // bool
 
+// Write decimal representation of signed integer 'x' to stdout.
+// Does not rely on stdio.h, uses OS function 'write'.
+// If 'term' != 0, append it.
+// Sets global 'errno' if write fails.
+// Return: sumber of bytes successfully written >= 0.
 static ssize_t printint(int x, const char term)
 {
     char buf[sizeof x * 4], *end = buf + sizeof buf, *pc = end;
-    const int sign = x < 0 ? -1 : 1;
+    const bool neg = x < 0;
     if (term)
         *--pc = term;
 	do {
         const div_t qr = div(x, 10);
-		*--pc = '0' + sign * qr.rem;  // remainder of negative number is negative
+		*--pc = '0' + abs(qr.rem);  // remainder of negative number is negative
 		x = qr.quot;
 	} while (x);
-    if (sign == -1)
+    if (neg)
         *--pc = '-';
     ssize_t n, sum = 0;
-    while (end - pc > 0 && (n = write(STDOUT_FILENO, pc, end - pc)) != -1) {  // may set errno
+    while (pc < end && (n = write(STDOUT_FILENO, pc, end - pc)) != -1) {
         sum += n;
         pc += n;
     }
     return sum;
 }
 
-// static char *sprintint(char *s, int x)
-// {
-//     char buf[sizeof x * 4];
-//     const int sign = x < 0 ? -1 : 1;
-//     int i = 0;
-// 	do {
-//         const div_t qr = div(x, 10);
-// 		buf[i++] = '0' + sign * qr.rem;  // remainder of negative number is negative
-// 		x = qr.quot;
-// 	} while (x);
-//     if (sign == -1)
-//         *s++ = '-';
-//     while (i)
-//         *s++ = buf[--i];
-//     return s;
-// }
+// Store decimal representation of signed integer 'x' in buffer 's' which
+// must have enough space (max 12 chars for 32-bit INT_MIN, including sign).
+// Return: pointer to next free character in s ('end' of number).
+static char *sprintint(char *s, int x)
+{
+    char buf[sizeof x * 4], *pc = buf;
+    const bool neg = x < 0;
+	do {
+        const div_t qr = div(x, 10);
+		*pc++ = '0' + abs(qr.rem);  // remainder of negative number is negative
+		x = qr.quot;
+	} while (x);
+    if (neg)
+        *s++ = '-';
+    while (pc > buf)
+        *s++ = *--pc;
+    return s;
+}
 
 int main(void)
 {
     printint(123, '\n');
     printint(-321, '\n');
-    // char buf[32] = "x=";
-    // char *end = sprintint(buf + 2, -456);
-    // *end = '\n';
-    // *(end + 1) = '\0';
-    // printf("%s", buf);
+
+    char buf[32] = "x=";
+    char *end = sprintint(buf + 2, -456);
+    *end++ = '\n';
+    *end++ = '\0';
+    printf("%s", buf);
     return 0;
 }
