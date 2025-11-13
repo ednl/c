5c0c5cb9919d90b78eb55d0105d2232eafbf5d5f
E. Dronkert <ednl@users.noreply.github.com>
2025-11-13 14:57:01 +0100

    error codes

--- sortoddeven.c
+++ sortoddeven.c
@@ -19,13 +19,20 @@
 #include <stdlib.h>
 #include <stdbool.h>
 
+typedef enum myerr {
+    E_OK = 0,  // no error
+    E_INV1,    // error: invalid int value on line 1
+    E_RANGE,   // error: value out of range on line 1
+    E_INV2,    // error: invalid int value on line 2
+} MyErr;
+
 #define MAXLEN 100
 static int data[MAXLEN];
 
 // Map [0,1] -> [-1,+1]
-static inline int asc(const bool cmp)
+static inline int asc(const bool state)
 {
-    return 2 * cmp - 1;
+    return 2 * state - 1;
 }
 
 // Compare two ints according to spec. Return:
@@ -38,24 +45,28 @@ static int spec(const void *p, const void *q)
     const int b = *(const int *)q;
     const bool aodd = a & 1;
     const bool bodd = b & 1;
-    if (aodd ^ bodd || a > b) return asc( aodd);
-    if (               a < b) return asc(!aodd);
-    return 0;
+    if (aodd ^ bodd || a > b) return asc( aodd);  // even before odd, odd: b before a, even: a before b
+    if (               a < b) return asc(!aodd);  // odd: a before b, even: b before a
+    return 0;  // a is b
 }
 
 int main(void)
 {
     int inputlen;
     if (scanf("%d", &inputlen) != 1)
-        return 1;
+        return E_INV1;
     if (inputlen < 1 || inputlen > MAXLEN)
-        return 2;
+        return E_RANGE;
     int datalen = 0;
-    for (int num; inputlen > 0 && scanf("%d", &num) == 1; --inputlen)
-        if (num >= 0)
+    for (int num; inputlen > 0; --inputlen) {
+        if (scanf("%d", &num) != 1)
+            return E_INV2;
+        if (num >= 0)  // only save non-negative numbers
             data[datalen++] = num;
+    }
     qsort(data, datalen, sizeof *data, spec);
     for (int i = 0; i < datalen; ++i)
         printf("%d ", data[i]);
     printf("\n");
+    return EXIT_SUCCESS;
 }
