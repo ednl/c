318e6bc3b7318e0b28babebbfc4d9dfc3f6a0fc6
E. Dronkert <ednl@users.noreply.github.com>
2026-01-26 11:02:01 +0100

    cleanup

--- modpow.c
+++ modpow.c
@@ -1,25 +1,25 @@
 #include <stdio.h>
-#include <stdint.h>  // uint64_t, UINT64_C
+#include <stdint.h>  // uint32_t, uint64_t, UINT64_C
 
 // Modular exponentiation: calculate remainder r = b^e mod m
-// Ref.: https://en.wikipedia.org/wiki/Modular_exponentiation
-uint32_t modpow(uint64_t base, uint64_t exponent, const uint64_t modulus)
+// Ref. https://en.wikipedia.org/wiki/Modular_exponentiation
+static uint32_t modpow(uint64_t base, uint64_t exponent, const uint64_t modulus)
 {
     if (modulus == 1)
         return 0;  // shortcut
     if (modulus == 0 || modulus > (UINT64_C(1) << 32))
         return -1;  // error (but also maximum unsigned value)
-    uint64_t r = 1;
+    uint64_t rem = 1;
     base %= modulus;  // now: base <= modulus-1
     for (; exponent; exponent >>= 1) {
         if (exponent & 1)  // current LSB set?
-            r = (r * base) % modulus;  // parentheses for clarity
+            rem = (rem * base) % modulus;  // parentheses for clarity
         base = (base * base) % modulus;  // overflow if base >= 1<<32
     }
-    return r;  // 0 <= r < 1<<32 because modulus <= 1<<32
+    return rem;  // 0 <= r < 1<<32 because modulus <= 1<<32
 }
 
 int main(void)
 {
-    printf("%d\n", modpow(4, 13, 497));  // 445
+    printf("%u\n", modpow(4, 13, 497));  // 445
 }
