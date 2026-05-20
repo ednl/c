a7a56677b9b0969ecd481e9fe2c53546e234c83c
E. Dronkert <ednl@users.noreply.github.com>
2026-05-20 08:46:48 +0200

    perm test

--- permutations-test.c
+++ permutations-test.c
@@ -9,8 +9,8 @@ int main(void)
     const int n = 13;
     int *a = NULL;
     uint64_t count = 0;
-    // while ((a = permutations(n))) {
-    while ((a = plainchanges(n))) {
+    // while ((a = permutations(n))) {  // M4: 14.4 s, M1: 24.7 s
+    while ((a = plainchanges(n))) {  // M4: 11.2 s, M1: ? s
         ++count;
         // printf("%3d:", ++count);
         // for (int i = 0; i < n; ++i)
@@ -18,6 +18,6 @@ int main(void)
         // printf("\n");
     }
     printf("%llu\n", count);
-    printf("Time: %.1f s\n", stoptimer_s());  // n=13 => t=24.7 s on Apple M1 3.2 GHz
+    printf("Time: %.1f s\n", stoptimer_s());
     return 0;
 }
