3c2b81b88e9156af5a97b90d36f0a5d94a467fa5
E. Dronkert <ednl@users.noreply.github.com>
2026-05-20 20:09:10 +0200

    timing & cleanup

--- permutations-test.c
+++ permutations-test.c
@@ -1,25 +1,32 @@
-// Times in milliseconds
-
+// Testing permutation algorithms
+// There are n-factorial possible permutations of an array with n elements.
+//   10! =     3,628,800
+//   11! =    39,916,800
+//   12! =   479,001,600
+//   13! = 6,227,020,800
+//
+// Times in milliseconds:
+//
 // Pi5 | count | lexic | plain |
 // ----+-------+-------+-------+
-//  10 |       |       |       |
-//  11 |       |       |       |
-//  12 |       |       |       |
-//  13 | 15600 | 51200 | 45600 |
-
+//  10 |     9 |    30 |    23 |
+//  11 |   100 |   325 |   263 |
+//  12 |  1200 |  3927 |  3556 |
+//  13 | 15594 | 50896 | 45340 |
+//
 //  M1 | count | lexic | plain |
 // ----+-------+-------+-------+
 //  10 |     7 |    15 |    10 |
 //  11 |    83 |   164 |   103 |
 //  12 |  1013 |  1977 |  1219 |
 //  13 | 13300 | 25773 | 15771 |
-
+//
 //  M4 | count | lexic | plain |
 // ----+-------+-------+-------+
-//  10 |       |       |       |
-//  11 |       |       |       |
-//  12 |       |       |       |
-//  13 |  1500 | 14300 | 11100 |
+//  10 |     1 |     8 |     7 |
+//  11 |     9 |    96 |    77 |
+//  12 |   109 |  1165 |   907 |
+//  13 |  1580 | 15257 | 11100 |
 
 #include <stdio.h>
 #include <stdlib.h>    // atoi
@@ -48,6 +55,7 @@ int main(int argc, char *argv[])
         return errmsg(argv[0], 2);
     if (!strchr("plc", argv[1][0]))
         return errmsg(argv[0], 3);
+
     const int n = atoi(argv[2]);
     if (n < 1 || n > 20)
         return errmsg(argv[0], 4);
@@ -56,9 +64,11 @@ int main(int argc, char *argv[])
     for (int i = 2; i <= n; ++i)
         fac *= i;
 
+    const char alg = argv[1][0];
     volatile uint64_t count = 0;
+
     starttimer();
-    switch (argv[1][0]) {
+    switch (alg) {
         case 'c': for (; count != fac; ++count); break;
         case 'l': for (int *a; (a = permutations(n)); ++count); break;
         case 'p': for (int *a; (a = plainchanges(n)); ++count); break;
