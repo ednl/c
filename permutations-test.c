ce4a0ce184bda09f31bce28f6a2d46f072c1edd5
E. Dronkert <ednl@users.noreply.github.com>
2026-05-20 16:57:44 +0200

    systematic timing

--- permutations-test.c
+++ permutations-test.c
@@ -1,24 +1,69 @@
+// Times in milliseconds
+
+// Pi5 | count | lexic | plain |
+// ----+-------+-------+-------+
+//  10 |       |       |       |
+//  11 |       |       |       |
+//  12 |       |       |       |
+//  13 | 15600 | 51200 | 45600 |
+
+//  M1 | count | lexic | plain |
+// ----+-------+-------+-------+
+//  10 |     7 |    15 |    10 |
+//  11 |    83 |   164 |   103 |
+//  12 |  1013 |  1977 |  1219 |
+//  13 | 13300 | 25773 | 15771 |
+
+//  M4 | count | lexic | plain |
+// ----+-------+-------+-------+
+//  10 |       |       |       |
+//  11 |       |       |       |
+//  12 |       |       |       |
+//  13 |  1500 | 14300 | 11100 |
+
 #include <stdio.h>
-#include <stdint.h>  // uint64_t
+#include <stdlib.h>    // atoi
+#include <string.h>    // strlen
+#include <stdint.h>    // uint64_t
 #include <inttypes.h>  // PRIu64
 #include "combperm.h"
 #include "startstoptimer.h"
 
-#define N 13
-#define PERM_PLAIN
-// #define PERM_LEXIC
-
-int main(void)
+static int errmsg(const char *const progname, const int errval)
 {
-    starttimer();
+    fprintf(stderr, "Test permutation algorithms\n");
+    fprintf(stderr, "Usage : %s p|l|c <n>\n", progname);
+    fprintf(stderr, "   p  : 'plain changes' (Steinhaus-Johnson-Trotter)\n");
+    fprintf(stderr, "   l  : lexicographic order\n");
+    fprintf(stderr, "   c  : only count to n factorial, no permutations\n");
+    fprintf(stderr, "  <n> : integer number >=1 and <=20 (practically <=13)\n");
+    return errval;
+}
+
+int main(int argc, char *argv[])
+{
+    if (argc != 3)
+        return errmsg(argv[0], 1);
+    if (strlen(argv[1]) != 1 || strlen(argv[2]) > 2)
+        return errmsg(argv[0], 2);
+    if (!strchr("plc", argv[1][0]))
+        return errmsg(argv[0], 3);
+    const int n = atoi(argv[2]);
+    if (n < 1 || n > 20)
+        return errmsg(argv[0], 4);
+
+    uint64_t fac = 1;
+    for (int i = 2; i <= n; ++i)
+        fac *= i;
+
     volatile uint64_t count = 0;
-#if defined PERM_PLAIN
-    for (int *a; (a = plainchanges(N)); ++count);  // M4: 11.2 s, M1: 15.7 s, Pi5: 44.5 s
-#elif defined PERM_LEXIC
-    for (int *a; (a = permutations(N)); ++count);  // M4: 14.4 s, M1: 25.4 s, Pi5: 51.2 s
-#else
-    for (; count < 6227020800U; ++count);          // M4: ? s, M1: 13.3 s, Pi5: ? s
-#endif
-    printf("%"PRIu64"\n", count);
-    printf("Time: %.1f s\n", stoptimer_s());
+    starttimer();
+    switch (argv[1][0]) {
+        case 'c': for (; count != fac; ++count); break;
+        case 'l': for (int *a; (a = permutations(n)); ++count); break;
+        case 'p': for (int *a; (a = plainchanges(n)); ++count); break;
+    }
+    const double t = stoptimer_ms();
+    printf("Count: %"PRIu64" (%d! = %"PRIu64")\n", count, n, fac);
+    printf("Time: %.0f ms\n", t);
 }
